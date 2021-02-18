d3871d9806df6703f384163900058e4ca5263ef2
tommitah <reijotahvanainen15@gmail.com>
2021-02-18 11:37:04 +0200

    first commit

--- src/pages/FormsPage/QuestionModule.js
+++ src/pages/FormsPage/QuestionModule.js
@@ -1,41 +1,39 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect } from "react"
+import { useState } from "react"
 
-const QuestionModule = ({update, questionIndex}) => {
-  const [input, setInput] = useState();
+const QuestionModule = ({ update, questionIndex }) => {
+  const [input, setInput] = useState()
   const [option, setOption] = useState("text")
 
   // const onInputChange = (e) => {
   //   setQuestions()
   // }
+  // Very commit
 
   useEffect(() => {
-    update({name: input, type: option}, questionIndex)
+    update({ name: input, type: option }, questionIndex)
   }, [input, option, questionIndex, update])
 
   // console.log(input);
-  return ( 
+  return (
     <>
-    <div>
-      <label>Question: </label>
-      <input
-        type="text"
-        name="question"
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <label style={{ padding: "1rem" }} align="center">
-        Choose
-      </label>
-      <select
-        value={option}
-        onChange={(e) => setOption(e.target.value)}
-      >
-        <option value="text">Textfield</option>
-        <option value="checkbox">Checkbox</option>
-      </select>
-    </div>
+      <div>
+        <label>Question: </label>
+        <input
+          type="text"
+          name="question"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <label style={{ padding: "1rem" }} align="center">
+          Choose
+        </label>
+        <select value={option} onChange={(e) => setOption(e.target.value)}>
+          <option value="text">Textfield</option>
+          <option value="checkbox">Checkbox</option>
+        </select>
+      </div>
     </>
-   );
+  )
 }
- 
-export default QuestionModule;
+
+export default QuestionModule
