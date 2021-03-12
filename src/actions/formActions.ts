fe499042cba8f5ecc99f914fbd87dd3597c15d29
wesenbergg <b.b.j.1998@gmail.com>
2021-03-12 15:22:21 +0200

    pari korjausta

--- src/actions/formActions.ts
+++ src/actions/formActions.ts
@@ -28,7 +28,7 @@ import {
 export const importForm = (file: any) => async (dispatch: any) => {
   let fr = new FileReader()
   fr.onloadend = (e) => {
-    const data = JSON.parse(fr.result)
+    const data = typeof fr.result === 'string' ? JSON.parse(fr.result): {}
     dispatch({ type: "SET_CURRENT_FORM", data })
   }
   fr.readAsText(file)
