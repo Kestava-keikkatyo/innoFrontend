7d8d63759910c8920759b4fc177bc88ee5bc551d
wesenbergg <b.b.j.1998@gmail.com>
2021-04-13 13:14:35 +0300

    examples

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,36 +1,60 @@
 import React from 'react'
-import { Card, CardContent, Typography } from '@material-ui/core'
+import { Card, CardContent, Grid, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core'
 
 const WhiteTypography = withStyles({
   root: {
-    color: "#FFFFFF"
-  }
+    color: '#FFFFFF',
+  },
 })(Typography)
 
 const LandingPage = () => {
-
   return (
     <div>
       <div>
         <div className="landing-banner" />
       </div>
-      <Card className="text-card" variant="outlined" style={{ backgroundColor: "black" }}>
+      <Card
+        className="text-card"
+        variant="outlined"
+        style={{ backgroundColor: 'black' }}
+      >
         <CardContent>
           <WhiteTypography align="center" variant="h1" gutterBottom>
             {'KeikkaKaveri'}
           </WhiteTypography>
-          <WhiteTypography variant="h4" gutterBottom>{'Kun henkilöstö voi hyvin, työ sujuu.'}</WhiteTypography>
+          <WhiteTypography variant="h4" gutterBottom>
+            {'Kun henkilöstö voi hyvin, työ sujuu.'}
+          </WhiteTypography>
           <WhiteTypography variant="h5" gutterBottom>
-            {'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'}
+            {
+              'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'
+            }
           </WhiteTypography>
         </CardContent>
       </Card>
-      <Card className="logo-card" variant="outlined" style={{ backgroundColor: "black" }}>
+      <Card
+        className="logo-card"
+        variant="outlined"
+        style={{ backgroundColor: 'black' }}
+      >
         <CardContent>
-          <WhiteTypography variant="h5" gutterBottom>{'Yhteistyössä: '}</WhiteTypography>
+          <WhiteTypography variant="h5" gutterBottom>
+            {'Yhteistyössä: '}
+          </WhiteTypography>
         </CardContent>
       </Card>
+      <div className="blank" />
+      <Grid container className="green">
+        <Grid item xs={12} md={6} className="part1">
+          Part1
+        </Grid>
+        <Grid item xs={12} md={6}>
+          Part2
+        </Grid>
+      </Grid>
+      <div className="blue" />
+      <div className="orange" />
     </div>
   )
 }
