598b8b93778cbc30906e18de03fd5f0ea53644f9
tommitah <reijotahvanainen15@gmail.com>
2021-04-13 12:10:12 +0300

    Added WhiteTypography, added "footer".

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/core'
 
+const WhiteTypography = withStyles({
+  root: {
+    color: "#FFFFFF"
+  }
+})(Typography)
 
 const LandingPage = () => {
 
@@ -9,9 +15,20 @@ const LandingPage = () => {
       <div>
         <div className="landing-banner" />
       </div>
-      <Card className="text-card" variant="outlined">
+      <Card className="text-card" variant="outlined" style={{ backgroundColor: "black" }}>
         <CardContent>
-          <Typography align="center" variant="h1">KeikkaKaveri</Typography>
+          <WhiteTypography align="center" variant="h1" gutterBottom>
+            {'KeikkaKaveri'}
+          </WhiteTypography>
+          <WhiteTypography variant="h4" gutterBottom>{'Kun henkilöstö voi hyvin, työ sujuu.'}</WhiteTypography>
+          <WhiteTypography variant="h5" gutterBottom>
+            {'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'}
+          </WhiteTypography>
+        </CardContent>
+      </Card>
+      <Card className="logo-card" variant="outlined" style={{ backgroundColor: "black" }}>
+        <CardContent>
+          <WhiteTypography variant="h5" gutterBottom>{'Yhteistyössä: '}</WhiteTypography>
         </CardContent>
       </Card>
     </div>
