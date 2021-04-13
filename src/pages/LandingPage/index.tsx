64957316de626325c7a6d1bc65391788fa3c07a3
tommitah <reijotahvanainen15@gmail.com>
2021-04-13 11:09:08 +0300

    Added content to the page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
 
 const LandingPage = () => {
 
   return (
     <div>
-      <div className="landing-banner" />
       <div>
-        
+        <div className="landing-banner" />
       </div>
+      <Card className="text-card" variant="outlined">
+        <CardContent>
+          <Typography align="center" variant="h1">KeikkaKaveri</Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
