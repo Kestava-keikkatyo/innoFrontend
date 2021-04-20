71c86f1ead9b09dbf7ab2fc9fbe8191f8cc4c040
tommitah <reijotahvanainen15@gmail.com>
2021-04-20 12:07:37 +0300

    Added image, moved assets around.

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardContent, Grid, Typography, Paper } from '@material-ui/core'
 import { withStyles } from '@material-ui/core'
+import logo from '../../assets/keikka-kaveri4.png'
 
 const WhiteTypography = withStyles({
   root: {
@@ -13,33 +14,45 @@ const LandingPage = () => {
     <div>
       <div className="landing-banner" />
       <div className="blank">
-        <Paper
-          className="text-card text-white"
-          style={{ backgroundColor: '#0007' }}
+        <Grid
+          container
+          direction="column"
+          justify="flex-start"
+          alignItems="flex-start"
+          spacing={10}
         >
-          <WhiteTypography align="center" variant="h1" gutterBottom>
-            {'KeikkaKaveri'}
-          </WhiteTypography>
-          <WhiteTypography variant="h4" gutterBottom>
-            {'Kun henkilöstö voi hyvin, työ sujuu.'}
-          </WhiteTypography>
-          <WhiteTypography variant="h5" gutterBottom>
-            {
-              'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'
-            }
-          </WhiteTypography>
-        </Paper>
-        <Card
-          className="logo-card"
-          variant="outlined"
-          style={{ backgroundColor: 'black' }}
-        >
-          <CardContent>
-            <WhiteTypography variant="h5" gutterBottom>
-              {'Yhteistyössä: '}
-            </WhiteTypography>
-          </CardContent>
-        </Card>
+          <Grid item xs={6} >
+            <Paper
+              className="text-card text-white"
+              elevation={3}
+              style={{ backgroundColor: '#0007' }}
+            >
+              <WhiteTypography align="center" variant="h1" gutterBottom>
+                {'KeikkaKaveri'}
+              </WhiteTypography>
+              <WhiteTypography variant="h4" gutterBottom>
+                {'Kun henkilöstö voi hyvin, työ sujuu.'}
+              </WhiteTypography>
+              <WhiteTypography variant="h5" gutterBottom>
+                {
+                  'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'
+                }
+              </WhiteTypography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} >
+            <Paper
+              className="text-car text-white"
+              elevation={3}
+              style={{ backgroundColor: '#0007' }}
+            >
+              <WhiteTypography variant="h5" gutterBottom>
+                {'Yhteistyössä: '}
+              </WhiteTypography>
+              <img className="logo" src={logo} />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
 
       <Grid container className="green">
