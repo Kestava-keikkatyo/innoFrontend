9800f228d019e759e0641eac1b501cfff817a511
wesenbergg <b.b.j.1998@gmail.com>
2021-05-01 00:08:38 +0300

    landingpage improvements

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
-import { Card, CardContent, Grid, Typography, Paper } from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  useTheme,
+  CardActions,
+  Button,
+  CardHeader,
+  MobileStepper,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core'
 import { withStyles } from '@material-ui/core'
+import SwipeableViews from 'react-swipeable-views'
+import Spacing from '../../components/Spacing'
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons'
 
 const WhiteTypography = withStyles({
   root: {
@@ -8,50 +24,163 @@ const WhiteTypography = withStyles({
   },
 })(Typography)
 
+const SimpleList = () => (
+  <>
+    <List component="nav" aria-label="main mailbox folders">
+      <ListItem button>
+        <ListItemText primary="Inbox" />
+      </ListItem>
+      <ListItem button>
+        <ListItemText primary="Drafts" />
+      </ListItem>
+    </List>
+    <List component="nav" aria-label="secondary mailbox folders">
+      <ListItem button>
+        <ListItemText primary="Trash" />
+      </ListItem>
+    </List>
+  </>
+)
+
+const CustomCard = () => (
+  <Card>
+    <CardHeader></CardHeader>
+    <CardContent>
+      <Typography color="textSecondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="h2">
+        be{}nev{}o{}lent
+      </Typography>
+      <Typography color="textSecondary">adjective</Typography>
+      <Typography variant="body2" component="p">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions disableSpacing>
+      <Button>Luo tili</Button>
+      <Button>Kirjaudu sisään</Button>
+    </CardActions>
+  </Card>
+)
+
 const LandingPage = () => {
+  const theme = useTheme()
+  const [activeStep, setActiveStep] = useState(0)
+
+  const handleStepChange = (step: any) => {
+    setActiveStep(step)
+  }
   return (
     <div>
-      <div className="landing-banner" />
-      <div className="blank">
-        <Paper
-          className="text-card text-white"
-          style={{ backgroundColor: '#0007' }}
-        >
-          <WhiteTypography align="center" variant="h1" gutterBottom>
-            {'KeikkaKaveri'}
-          </WhiteTypography>
-          <WhiteTypography variant="h4" gutterBottom>
-            {'Kun henkilöstö voi hyvin, työ sujuu.'}
-          </WhiteTypography>
-          <WhiteTypography variant="h5" gutterBottom>
-            {
-              'KeikkaKaveri tarjoaa tietoa, koulutusta ja välineitä turvallisten ja terveellisten työolojen kehittämiseen ja ylläpitämiseen.'
-            }
-          </WhiteTypography>
-        </Paper>
-        <Card
-          className="logo-card"
-          variant="outlined"
-          style={{ backgroundColor: 'black' }}
-        >
-          <CardContent>
-            <WhiteTypography variant="h5" gutterBottom>
-              {'Yhteistyössä: '}
-            </WhiteTypography>
-          </CardContent>
-        </Card>
-      </div>
-
-      <Grid container className="green">
-        <Grid item xs={12} md={6} className="part1">
-          Part1
+      <MobileStepper
+        variant="dots"
+        steps={4}
+        position="static"
+        activeStep={activeStep}
+        className="landing-stepper"
+        nextButton={
+          <Button size="small" disabled={activeStep === 3}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+          </Button>
+        }
+      />
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+        className="swipable-card-container"
+      >
+        <Grid item xs={12} className="landing-part1">
+          <div className="landing-banner" />
         </Grid>
-        <Grid item xs={12} md={6}>
-          Part2
+        <Grid container className="landing-part2">
+          <Grid item xs={12} md={6}>
+            Part1
+          </Grid>
+          <Grid item xs={12} md={6} className="bg-white">
+            Part2
+          </Grid>
         </Grid>
-      </Grid>
-      <div className="blue" />
-      <div className="orange" />
+        <Grid container className="landing-part3">
+          <Grid item xs={12} className="landing-title-container">
+            <Typography variant="h4" color="secondary">
+              Vastuualueet
+            </Typography>
+            <Button variant="contained" color="secondary">
+              Lue lisää
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            Part3
+          </Grid>
+          <Grid item xs={12} md={6}>
+            Part4
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          direction="column"
+          alignItems="stretch"
+          justify="flex-start"
+          className="landing-part4"
+        >
+          <Grid item xs={12} className="landing-part41 bg-white">
+            <Grid container>
+              <Grid item xs={4}>
+                <Spacing m5 p5>
+                  <Typography>Oletko työntekijä?</Typography>
+                  <CustomCard />
+                </Spacing>
+              </Grid>
+              <Grid item xs={4}>
+                <Spacing m5 p5>
+                  <Typography>Oletko käyttäjäyritys?</Typography>
+                  <CustomCard />
+                </Spacing>
+              </Grid>
+              <Grid item xs={4}>
+                <Spacing m5 p5>
+                  <Typography>Oletko vuokratyöfirma?</Typography>
+                  <CustomCard />
+                </Spacing>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} className="landing-part42">
+            <Grid container>
+              <Grid xs={3} item>
+                <SimpleList />
+              </Grid>
+              <Grid xs={3} item>
+                <SimpleList />
+              </Grid>
+              <Grid xs={3} item>
+                <SimpleList />
+              </Grid>
+              <Grid xs={3} item>
+                <SimpleList />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </SwipeableViews>
     </div>
   )
 }
