1635c69c86d3467f2f00c9bc4ac03eb66ea3e823
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-07-04 15:51:22 +0300

    baseUrl.ts updated

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -2,6 +2,6 @@
 let baseUrl = 'http://localhost:3001/api';
 
 if(process.env.NODE_ENV === 'production'){
-  baseUrl = 'http://';
+  baseUrl = 'http://3.65.218.23:3001/api';
 }
  export default baseUrl;
