05bc4efcf2185320989b7d199cd287624380f13d
wesenbergg <b.b.j.1998@gmail.com>
2021-08-08 00:58:36 +0300

    -m

--- src/pages/BusinessContractsPage/AgenciesList.tsx
+++ src/pages/BusinessContractsPage/AgenciesList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import {
   Box,
   InputBase,
@@ -7,15 +7,15 @@ import {
   useMediaQuery,
   InputLabel,
   Select,
-  MenuItem
-} from "@material-ui/core";
-import { Search as SearchIcon } from "@material-ui/icons";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAgencies, fetchAllAgencies } from "../../actions/allUsersActions";
-import { IRootState } from "../../utils/store";
-import AgencyCard from "./AgencyCard";
-import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
-import {  Theme, useTheme } from "@material-ui/core/styles";
+  MenuItem,
+} from '@material-ui/core'
+import { Search as SearchIcon } from '@material-ui/icons'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAgencies, fetchAllAgencies } from '../../actions/allUsersActions'
+import { IRootState } from '../../utils/store'
+import AgencyCard from './AgencyCard'
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
+import { Theme, useTheme } from '@material-ui/core/styles'
 
 /**
  * @component
@@ -23,110 +23,115 @@ import {  Theme, useTheme } from "@material-ui/core/styles";
  * A searchbar for searching worker users or business users.
  */
 const AgenciesList = () => {
-  const dispatch = useDispatch();
-  const [input, setInput] = useState('');
-  const { agencies } = useSelector((state: IRootState) => state.allUsers);
+  const dispatch = useDispatch()
+  const [input, setInput] = useState('')
+  const { agencies } = useSelector((state: IRootState) => state.allUsers)
   const [allAgencies, setAllAgencies] = useState(agencies)
-  const [alignment, setAlignment] = React.useState('Kaikki');
+  const [alignment, setAlignment] = React.useState('Kaikki')
   const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
-
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
 
   useEffect(() => {
-    dispatch(fetchAllAgencies());
-  }, [dispatch]);
+    dispatch(fetchAllAgencies())
+  }, [dispatch])
 
-  const handleChange = (event: React.MouseEvent<HTMLElement>, value: string) => {
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    value: string
+  ) => {
     event.preventDefault()
     setAlignment(value)
     if (alignment === 'Kaikki') {
       setAllAgencies([])
     } else {
-      const result = agencies.filter((agency: any) => agency.category === value);
+      const result = agencies.filter((agency: any) => agency.category === value)
       setAllAgencies(result)
     }
-  };
+  }
 
   //Iniates search query of Agencies
   const handleSubmit = (event: any) => {
-    event.preventDefault();
+    event.preventDefault()
     if (input.length > 0) {
-      dispatch(fetchAgencies(input));
+      dispatch(fetchAgencies(input))
       setAllAgencies(agencies)
     }
-  };
+  }
 
-  if(matches) {
+  if (matches) {
     return (
       <div>
-        <FormControl
-        >
-        <InputLabel
-        
-        >
-        <Select
-          value={alignment}
-          onChange={handleChange}
-          >
-        <MenuItem value='Kaikki'>Kaikki</MenuItem>
-        <MenuItem value='IT- ja tietoliikenne'>IT- ja tietoliikenne</MenuItem>
-        </Select>
-        </InputLabel>
+        <FormControl>
+          <InputLabel>
+            <Select
+              value={alignment}
+              onChange={({ target }: any) => setAlignment(target.value)}
+            >
+              <MenuItem value="Kaikki">Kaikki</MenuItem>
+              <MenuItem value="IT- ja tietoliikenne">
+                IT- ja tietoliikenne
+              </MenuItem>
+              <MenuItem value="Koulutus- ja opetusala">
+                Koulutus- ja opetusala
+              </MenuItem>
+              <MenuItem value="Tekniikka">Tekniikka</MenuItem>
+              <MenuItem value="Lääketeollisuus- ja farmasia">
+                Lääketeollisuus- ja farmasia
+              </MenuItem>
+              <MenuItem value="Kiinteistö">Kiinteistö</MenuItem>
+            </Select>
+          </InputLabel>
         </FormControl>
- 
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-        alignItems="center"
-        flexWrap="wrap"
-      >
-        <FormControl component="fieldset"></FormControl>
-        <form onSubmit={handleSubmit}>
-          <Box display="flex" alignItems="center">
-            <InputBase
-              placeholder="search with name"
-              value={input}
-              onChange={({ target }) => setInput(target.value)}
-            />
-            <IconButton type="submit">
-              <SearchIcon />
-            </IconButton>
-          </Box>
-        </form>
-      </Box>
-      {allAgencies.length > 0 ? allAgencies.map((agency: any) => (
-        <AgencyCard key={agency._id} agency={agency} />
-      )):agencies.map((agency: any) => (
-        <AgencyCard key={agency._id} agency={agency} />
-      ))}
-    </div>
+
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+          flexWrap="wrap"
+        >
+          <FormControl component="fieldset"></FormControl>
+          <form onSubmit={handleSubmit}>
+            <Box display="flex" alignItems="center">
+              <InputBase
+                placeholder="search with name"
+                value={input}
+                onChange={({ target }) => setInput(target.value)}
+              />
+              <IconButton type="submit">
+                <SearchIcon />
+              </IconButton>
+            </Box>
+          </form>
+        </Box>
+        {allAgencies.length > 0
+          ? allAgencies.map((agency: any) => (
+              <AgencyCard key={agency._id} agency={agency} />
+            ))
+          : agencies.map((agency: any) => (
+              <AgencyCard key={agency._id} agency={agency} />
+            ))}
+      </div>
     )
   }
 
   return (
     <div>
       <ToggleButtonGroup value={alignment} exclusive onChange={handleChange}>
-        <ToggleButton value="Kaikki">
-          Kaikki
-        </ToggleButton>
+        <ToggleButton value="Kaikki">Kaikki</ToggleButton>
         <ToggleButton value="Rakennus, asennus ja huolto">
           Rakennus, asennus ja huolto
         </ToggleButton>
         <ToggleButton value="IT- ja tietoliikenne">
           IT- ja tietoliikenne
-        </ToggleButton >
+        </ToggleButton>
         <ToggleButton value="Koulutus- ja opetusala">
           Koulutus- ja opetusala
-        </ToggleButton >
-        <ToggleButton value="Tekniikka">
-          Tekniikka
         </ToggleButton>
+        <ToggleButton value="Tekniikka">Tekniikka</ToggleButton>
         <ToggleButton value="Lääketeollisuus- ja farmasia">
           Lääketeollisuus- ja farmasia
         </ToggleButton>
-        <ToggleButton value="Kiinteistö">
-          Kiinteistö
-        </ToggleButton>
+        <ToggleButton value="Kiinteistö">Kiinteistö</ToggleButton>
       </ToggleButtonGroup>
       <Box
         display="flex"
@@ -148,13 +153,15 @@ const AgenciesList = () => {
           </Box>
         </form>
       </Box>
-      {allAgencies.length > 0 ? allAgencies.map((agency: any) => (
-        <AgencyCard key={agency._id} agency={agency} />
-      )):agencies.map((agency: any) => (
-        <AgencyCard key={agency._id} agency={agency} />
-      ))}
+      {allAgencies.length > 0
+        ? allAgencies.map((agency: any) => (
+            <AgencyCard key={agency._id} agency={agency} />
+          ))
+        : agencies.map((agency: any) => (
+            <AgencyCard key={agency._id} agency={agency} />
+          ))}
     </div>
-  );
-};
+  )
+}
 
-export default AgenciesList;
+export default AgenciesList
