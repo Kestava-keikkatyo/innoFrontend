7bc5423958b539c7e29b09cbc7ff162a85c567e8
Walter R <waltsu123123@gmail.com>
2021-08-10 13:09:41 +0300

    AgencyCard muutettu accordion tyyliseksi

--- src/pages/BusinessContractsPage/AgencyCard.tsx
+++ src/pages/BusinessContractsPage/AgencyCard.tsx
@@ -1,18 +1,111 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import Button from '@material-ui/core/Button';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar, Button, Card, CardContent, CardHeader, Collapse, Grid, IconButton, Typography, CardActions } from '@material-ui/core';
 
 import CooperationInfoModal from './CooperationInfoModal';
-import { useHistory } from 'react-router';
+import { red } from '@material-ui/core/colors';
+
+
+interface Agency {
+  businessContracts: [string];
+  name: string;
+  email: string;
+  profile: string;
+}
+
+export const AgencyCards = (prop: { agency: Agency }) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const classes = useStyles();
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const { agency } = prop;
+  const [displayModal, setDisplayModal] = React.useState(false);
+
+  const handleCooperationOpen = () => {
+    setDisplayModal(true);
+  };
+
+  const contractId = agency.businessContracts[0];
+
+  return (
+    <div>
+    <Card className={classes.root} onClick={handleExpandClick}>
+      <CardHeader
+        avatar={
+          <Avatar
+            aria-label="recipe"
+            className={classes.avatar}
+            src={"profile.profilePicture"}
+            alt="profilePicture"
+          />
+        }
+        action={
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        }
+        title={agency.name}
+        subheader={agency.email}
+      />
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Grid container spacing={0}>
+          <Grid className={classes.gridButton} item sm={4} xs={12}>
+            <CardActions>
+              <div className={classes.buttons}>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+            >
+              Siirry yrityksen profiiliin
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              type="button"
+              onClick={handleCooperationOpen}
+            >
+              Lähetä yhteistyökutsu
+            </Button>
+            </div>
+            </CardActions>
+          </Grid>
+          <Grid className={classes.gridText} item sm={8} xs={12}>
+            <Typography variant="body2" color="textSecondary" component="p">
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like. This impressive paella is a perfect
+              party dish and a fun meal to cook together with your guests. Add
+              200 cups of frozen peanuts along with the mussels, if you like.
+            </Typography>
+          </Grid>
+        </Grid> 
+        </CardContent>
+      </Collapse>
+    </Card>
+        <CooperationInfoModal
+        displayModal={displayModal}
+        agency={agency}
+        contractId={contractId}
+        closeModal={() => setDisplayModal(false)}
+      />
+      </div>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -48,7 +141,7 @@ const useStyles = makeStyles((theme) => ({
   },
   gridText: {
     textAlign: 'center',
-    paddingTop: '1.125em',
+    paddingTop: '1.4em',
     paddingRight: '5em',
     [theme.breakpoints.down('xs')]: {
       paddingRight: 0,
@@ -57,100 +150,14 @@ const useStyles = makeStyles((theme) => ({
   content: {
     paddingTop: '0',
   },
-  button: {
-    width: '80%',
-    marginTop: '0.3125em',
+  buttons: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
   },
+  button: {
+    marginBottom: '1.5%'
+  }
 }));
 
-interface Agency {
-  businessContracts: [string];
-  name: string;
-  email: string;
-  profile: string;
-}
-
-export const AgencyCard = (prop: { agency: Agency }) => {
-  const classes = useStyles();
-  const { agency } = prop;
-
-  const history = useHistory();
-
-  const [displayModal, setDisplayModal] = React.useState(false);
-
-  const handleOpen = () => {
-    setDisplayModal(true);
-  };
-
-  const contractId = agency.businessContracts[0];
-
-  const transferToAgencyPage = (profileId: any) => {
-    history.push({
-      pathname: "profile-view",
-      state: { profileId: profileId },
-    });
-  };
-
-
-
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        className={classes.header}
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={agency.name}
-        subheader={agency.email}
-      />
-      
-      <CardContent className={classes.content}>
-        <Grid container spacing={0}>
-          <Grid className={classes.gridButton} item sm={4} xs={12}>
-            <Button
-              className={classes.button}
-              variant="contained"
-              color="primary"
-              onClick={() => transferToAgencyPage(agency.profile)}
-            >
-              Siirry yrityksen profiiliin
-            </Button>
-            <Button
-              className={classes.button}
-              variant="contained"
-              color="primary"
-              type="button"
-              onClick={handleOpen}
-            >
-              Lähetä yhteistyökutsu
-            </Button>
-            <CooperationInfoModal
-              displayModal={displayModal}
-              agency={agency}
-              contractId={contractId}
-              closeModal={() => setDisplayModal(false)}
-            />
-          </Grid>
-          <Grid className={classes.gridText} item sm={8} xs={12}>
-            <Typography variant="body2" color="textSecondary" component="p">
-              This impressive paella is a perfect party dish and a fun meal to
-              cook together with your guests. Add 1 cup of frozen peas along
-              with the mussels, if you like. This impressive paella is a perfect
-              party dish and a fun meal to cook together with your guests. Add
-              200 cups of frozen peanuts along with the mussels, if you like.
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
-};
-
-export default AgencyCard;
+export default AgencyCards;
