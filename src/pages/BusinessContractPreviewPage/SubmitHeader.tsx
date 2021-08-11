5f5cab83bb8e74f6e1627fafafebb730918295b0
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-08-11 23:47:21 +0300

    Bug fixes to make business contracts with businesses and workers

--- src/pages/BusinessContractPreviewPage/SubmitHeader.tsx
+++ src/pages/BusinessContractPreviewPage/SubmitHeader.tsx
@@ -1,49 +1,54 @@
-import { Button, Grid, Typography } from '@material-ui/core'
-import React from 'react'
-import {useDispatch, useSelector } from 'react-redux'
-import { Link, useHistory} from 'react-router-dom'
-import { submitForm} from '../../actions/businessContractFormActions'
-import { IRootState } from '../../utils/store'
+import { Button, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { submitForm } from '../../actions/businessContractFormActions';
+import { IRootState } from '../../utils/store';
 
 /**
  * @component
  * @desc A header for preview page.
  */
 const SubmitHeader: React.FC<any> = () => {
+  const currentBusinssContractForm = useSelector(
+    (state: IRootState) => state.businessContractForm
+  );
 
-  const currentBusinssContractForm = useSelector((state: IRootState) => state.businessContractForm)
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const history:any = useHistory()
+  const history: any = useHistory();
 
   const handleSubmit = () => {
-    currentBusinssContractForm.filled = true
-    dispatch(submitForm(currentBusinssContractForm, history.location.state.contractId))
-    history.push(`/business-contracts`)
-
-  }
+    const copyOfCurrentBusinssContractForm = {
+      ...currentBusinssContractForm,
+      filled: true,
+    };
+    dispatch(
+      submitForm(
+        copyOfCurrentBusinssContractForm,
+        history.location.state.contractId
+      )
+    );
+    history.push(`/business-contracts`);
+  };
 
   return (
-    <Grid container direction="row"
-      justify="space-between">
-        <Grid item xs={6}>
-          <Typography variant="h4" color="secondary" >
-            Fill Business Contract Form
-          </Typography>
-        </Grid>
-        <Grid item xs={6} >
-          <Grid container direction="row-reverse">
-            <Button>
-              <Link to="/business-contracts">Back</Link>
-            </Button>
-            <Button onClick={handleSubmit}>
-              Submit
-            </Button>
-          </Grid>
+    <Grid container direction="row" justify="space-between">
+      <Grid item xs={6}>
+        <Typography variant="h4" color="secondary">
+          Fill Business Contract Form
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid container direction="row-reverse">
+          <Button>
+            <Link to="/business-contracts">Back</Link>
+          </Button>
+          <Button onClick={handleSubmit}>Submit</Button>
         </Grid>
       </Grid>
-  )
-}
+    </Grid>
+  );
+};
 
-export default SubmitHeader
+export default SubmitHeader;
--- src/pages/BusinessContractsPage/ListAccordionInBox.tsx
+++ src/pages/BusinessContractsPage/ListAccordionInBox.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import {
   Accordion,
   AccordionDetails,
@@ -12,24 +12,27 @@ import {
   useMediaQuery,
   useTheme,
   Button,
-} from "@material-ui/core";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import Avatar from "@material-ui/core/Avatar";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
-import { sendBusinessContract, refuseBusinessContractById} from "../../actions/businessContractActions";
-import { getFormById } from "../../actions/formActions";
-import { IRootState } from "../../utils/store";
-import { getFormByIdAndSetBusinessContractForm} from "../../actions/businessContractFormActions";
-import { severity } from "../../types/types";
-import { setAlert } from "../../actions/alertActions";
-import formServices from "../../services/formServices"
+import Avatar from '@material-ui/core/Avatar';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import {
+  sendBusinessContract,
+  refuseBusinessContractById,
+} from '../../actions/businessContractActions';
+import { getFormById } from '../../actions/formActions';
+import { IRootState } from '../../utils/store';
+import { getFormByIdAndSetBusinessContractForm } from '../../actions/businessContractFormActions';
+import { severity } from '../../types/types';
+import { setAlert } from '../../actions/alertActions';
+import formServices from '../../services/formServices';
 import pdfMake from 'pdfmake/build/pdfmake.js';
 import pdfFonts from 'pdfmake/build/vfs_fonts.js';
-import htmlToPdfmake from 'html-to-pdfmake'
-import ReactDOMServer from "react-dom/server";
-import Form from "../FormsPage/Form";
+import htmlToPdfmake from 'html-to-pdfmake';
+import ReactDOMServer from 'react-dom/server';
+import Form from '../FormsPage/Form';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 
 import SendIcon from '@material-ui/icons/Send';
@@ -43,118 +46,123 @@ import Tooltip from '@material-ui/core/Tooltip';
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
-    width: "100%",
-    backgroundColor: theme.palette.background.paper
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
   },
-  logoColumn:{
-    flexBasis: '20%'
+  logoColumn: {
+    flexBasis: '20%',
   },
   column: {
     flexBasis: '40%',
-    wordWrap:'break-word',
-    marginLeft:'10px'
+    wordWrap: 'break-word',
+    marginLeft: '10px',
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
   },
   color: {
-    color: "red",
+    color: 'red',
   },
   info: {
-    display:'column',
-    width: "30rem",
-  }
-
+    display: 'column',
+    width: '30rem',
+  },
 }));
 
 export const ListAccordionInBox = (prop: { contracts: any[] }) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
-  const theme = useTheme()
+  const theme = useTheme();
 
-  const matches = useMediaQuery(theme.breakpoints.down('sm'))
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
-  console.log("matches", matches)
+  console.log('matches', matches);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const history = useHistory()
+  const history = useHistory();
 
-  const { contracts } = prop
+  const { contracts } = prop;
 
-  const currentForm:any = useSelector((state: IRootState ) => state.form)
+  const currentForm: any = useSelector((state: IRootState) => state.form);
 
-  const currentBusinessContractForm:any = useSelector((state: IRootState ) => state.businessContractForm)
+  const currentBusinessContractForm: any = useSelector(
+    (state: IRootState) => state.businessContractForm
+  );
 
-  const handleEsitteleLomaketta = (formId:any) => {
-
-    if(!currentBusinessContractForm._id || currentBusinessContractForm._id === ''){
+  const handleEsitteleLomaketta = (formId: any) => {
+    if (
+      !currentBusinessContractForm._id ||
+      currentBusinessContractForm._id === ''
+    ) {
       //dispatch(getFormById(formId))
-      dispatch(getFormByIdAndSetBusinessContractForm(formId))
+      dispatch(getFormByIdAndSetBusinessContractForm(formId));
     }
-    history.push(`/business-contracts/business-contract-preview`)
+    history.push(`/business-contracts/business-contract-preview`);
+  };
 
-  }
-
-  const handleTäytäLomaketta =  (formId:any, contractId:string) => {
-
-    if(currentBusinessContractForm.filled){
-      dispatch(
-        setAlert("Lomake on jo täydetty" , severity.Error)
-        )
-    }else{
+  const handleTäytäLomaketta = (formId: any, contractId: string) => {
+    console.log('handleTäytäLomaketta: formId', formId);
+    console.log('handleTäytäLomaketta: contractId', contractId);
+    if (currentBusinessContractForm.filled) {
+      dispatch(setAlert('Lomake on jo täydetty', severity.Error));
+    } else {
       //dispatch(getFormById(formId))
-      dispatch(getFormByIdAndSetBusinessContractForm(formId))
-      history.push({pathname: `/business-contracts/business-contract-fill`,state: { contractId: contractId}})
+      dispatch(getFormByIdAndSetBusinessContractForm(formId));
+      history.push({
+        pathname: `/business-contracts/business-contract-fill`,
+        state: { contractId: contractId },
+      });
       //history.push(`/business-contract-fill`,{ params: contractId})
     }
+  };
 
-  }
+  const handleMuokkaaTäytettyäLomaketta = () => {
+    if (currentBusinessContractForm.filled)
+      history.push(`/business-contracts/business-contract-edit`);
+    else dispatch(setAlert('Lomake ei ole vielä täydetty', severity.Error));
+  };
 
-  const handleMuokkaaTäytettyäLomaketta =  () => {
-
-    if(currentBusinessContractForm.filled)
-      history.push(`/business-contracts/business-contract-edit`)
-    else
-      dispatch(
-      setAlert("Lomake ei ole vielä täydetty" , severity.Error)
-      )
-  }
-
-  const rejectContract = (agencyId:any, contractId:any, formId:any) => {
-    dispatch(getFormById(formId))
+  const rejectContract = (agencyId: any, contractId: any, formId: any) => {
+    dispatch(getFormById(formId));
     if (window.confirm(`Poistetaanko ${currentForm.title}`)) {
-      dispatch(refuseBusinessContractById(agencyId,contractId));
+      dispatch(refuseBusinessContractById(agencyId, contractId));
     }
-  }
+  };
 
-  const loadAndSendContract = (contractId:any) => {
-        dispatch(sendBusinessContract(contractId, currentBusinessContractForm._id))
-  }
+  const loadAndSendContract = (agencyId: any, contractId: any) => {
+    dispatch(
+      sendBusinessContract(
+        agencyId,
+        contractId,
+        currentBusinessContractForm._id
+      )
+    );
+  };
 
   // Print PDF
-  const handleTulostaLomaketta =  async (formId:any) => {
-    let form:any = await formServices.fetchFormById(formId)
-    console.log("form ", form)
+  const handleTulostaLomaketta = async (formId: any) => {
+    let form: any = await formServices.fetchFormById(formId);
+    console.log('form ', form);
 
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
     // pdf content
-    let content:any = []
+    let content: any = [];
 
-    let html = ReactDOMServer.renderToString(<Form currentForm={form}/>)
-    let htmlForm:any = htmlToPdfmake(html);
+    let html = ReactDOMServer.renderToString(<Form currentForm={form} />);
+    let htmlForm: any = htmlToPdfmake(html);
 
-    content.push(htmlForm)
+    content.push(htmlForm);
 
     // pdf document
     var doc = {
-        content: content
+      content: content,
     };
 
     pdfMake.createPdf(doc).download(form.title);
-  }
+  };
 
   if (contracts.length < 1) {
     return <p>no results</p>;
@@ -172,7 +180,9 @@ export const ListAccordionInBox = (prop: { contracts: any[] }) => {
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
               </div>
               <div className={classes.column}>
-                <Typography className={classes.heading}>{contract.agency.name}</Typography>
+                <Typography className={classes.heading}>
+                  {contract.agency.name}
+                </Typography>
               </div>
               <div className={classes.column}>
                 <Typography className={classes.color}>
@@ -182,58 +192,92 @@ export const ListAccordionInBox = (prop: { contracts: any[] }) => {
             </AccordionSummary>
             <AccordionDetails>
               <div className={classes.info}>
-                <Typography style={{margin:'10px 5px'}}>Email: {contract.agency.email}</Typography>
+                <Typography style={{ margin: '10px 5px' }}>
+                  Email: {contract.agency.email}
+                </Typography>
                 <Divider />
-                <Typography style={{margin:'10px 5px'}}>
+                <Typography style={{ margin: '10px 5px' }}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
                   eget.
                 </Typography>
-                <Button style={{margin:'5px'}} color='primary' variant='contained'>
+                <Button
+                  style={{ margin: '5px' }}
+                  color="primary"
+                  variant="contained"
+                >
                   Yrityksen Nettisivu
                 </Button>
-
               </div>
             </AccordionDetails>
 
             <AccordionActions disableSpacing>
-
               <Tooltip title="Hylkää Sopimus" placement="top" arrow>
-                <IconButton onClick={() => rejectContract(contract.agency._id,contract._id, contract.pendingContracts.formId)}>
+                <IconButton
+                  onClick={() =>
+                    rejectContract(
+                      contract.agency._id,
+                      contract._id,
+                      contract.pendingContracts.formId
+                    )
+                  }
+                >
                   <CloseIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Esikatsele Lomakettä" placement="top" arrow>
-                <IconButton onClick={() => handleEsitteleLomaketta(contract.pendingContracts.formId)}>
+                <IconButton
+                  onClick={() =>
+                    handleEsitteleLomaketta(contract.pendingContracts.formId)
+                  }
+                >
                   <VisibilityIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Täytä lomaketta" placement="top" arrow>
-                <IconButton onClick={() => handleTäytäLomaketta(contract.pendingContracts.formId,contract._id)}>
+                <IconButton
+                  onClick={() =>
+                    handleTäytäLomaketta(
+                      contract.pendingContracts.formId,
+                      contract._id
+                    )
+                  }
+                >
                   <ListAltIcon />
                 </IconButton>
               </Tooltip>
 
-              <Tooltip title="Muokkaa Täytettyä lomaketta" placement="top" arrow>
+              <Tooltip
+                title="Muokkaa Täytettyä lomaketta"
+                placement="top"
+                arrow
+              >
                 <IconButton onClick={handleMuokkaaTäytettyäLomaketta}>
                   <EditIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Tulosta pdf" placement="top" arrow>
-                <IconButton onClick={() => handleTulostaLomaketta(contract.pendingContracts.formId)}>
+                <IconButton
+                  onClick={() =>
+                    handleTulostaLomaketta(contract.pendingContracts.formId)
+                  }
+                >
                   <SaveAltIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Lähetä Sopimus" placement="top" arrow>
-                <IconButton onClick={() => loadAndSendContract(contract._id)}>
+                <IconButton
+                  onClick={() =>
+                    loadAndSendContract(contract.agency._id, contract._id)
+                  }
+                >
                   <SendIcon />
                 </IconButton>
               </Tooltip>
-
             </AccordionActions>
           </Accordion>
         ))}
