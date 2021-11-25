7acde8042a3ea7e72d99e2cdd6bbcf681ceaa752
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-11-25 14:57:47 +0200

    Added save button to edit profile header

--- src/pages/ProfilePage/EditProfileComponents/EditProfileHeader.tsx
+++ src/pages/ProfilePage/EditProfileComponents/EditProfileHeader.tsx
@@ -2,21 +2,23 @@ import { Grid, Button, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { useTranslation } from 'react-i18next';
 /**
  * @component
  * @desc Renders edit profile page header
  */
-const EditProfileHeader: React.FC<any> = () => {
+const EditProfileHeader: React.FC<any> = ({ submitProfile, savingChanges }) => {
   const history = useHistory();
   const classes = useStyles();
+  const { t } = useTranslation();
 
   return (
     <Grid
       container
       direction="row"
       justify="space-between"
-      style={{ marginTop: 10, marginBottom: 10 }}
+      style={{ marginTop: 20, marginBottom: 10 }}
     >
       <Grid item xs={6}>
         <Button
@@ -31,6 +33,19 @@ const EditProfileHeader: React.FC<any> = () => {
           >
             Return
           </Link>
+        </Button>
+      </Grid>
+      {/* ### Submit button ### */}
+      <Grid item xs={6}>
+        <Button
+          variant="contained"
+          type="submit"
+          color="primary"
+          className={classes.button}
+          onClick={submitProfile}
+          style={{ minWidth: 200, float: 'right' }}
+        >
+          {savingChanges ? <CircularProgress size={24} /> : t('save_changes')}
         </Button>
       </Grid>
     </Grid>
