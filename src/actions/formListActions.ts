261af4d5ecd2b69a4f795732f8fba87981bfcf0d
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-11-25 14:00:08 +0200

    Common forms bug fixed

--- src/actions/formListActions.ts
+++ src/actions/formListActions.ts
@@ -14,7 +14,6 @@ export const fetchFormList = () => async (dispatch: any) => {
   const data1 = await formServices.fetchMyFormList()
   const data2 = await formServices.fetchCommunityFormList()
   const data3 = await formServices.fetchCommonFormList()
-  if (!data1) return
   dispatch({
     type: FETCH_FORM_LIST,
     data: { myForms: data1, communityForms: data2, commonForms: data3 },
@@ -29,7 +28,7 @@ export const addToFormList = (form: Form) => async (dispatch: any) => {
  * @function
  * @desc Updates searchlist
  */
- export const updatFormList = (input: string, formType: any) => async (dispatch: any) => {
+export const updatFormList = (input: string, formType: any) => async (dispatch: any) => {
   //const res = await formServices.searchForms(input, formType)
   //dispatch({ type: F_UPDATE, data: res.data })
 }
--- src/pages/FormsPage/MyFormsTable.tsx
+++ src/pages/FormsPage/MyFormsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 import {
   TableContainer,
@@ -18,113 +18,105 @@ import {
   makeStyles,
   useTheme,
   useMediaQuery,
-  Tooltip
-} from '@material-ui/core'
-import { useSelector } from 'react-redux'
+  Tooltip,
+} from '@material-ui/core';
+import { useSelector } from 'react-redux';
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   AccordionActions,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import EditIcon from '@material-ui/icons/Edit'
+import EditIcon from '@material-ui/icons/Edit';
 //import MoveToInboxIcon from '@material-ui/icons/MoveToInbox'
 import DeleteIcon from '@material-ui/icons/Delete';
 import SaveAltIcon from '@material-ui/icons/SaveAlt';
 
-import { useDispatch } from "react-redux"
-import { DeleteFormById, getFormById } from "../../actions/formActions"
-import { useHistory } from "react-router"
-import formServices from "../../services/formServices"
+import { useDispatch } from 'react-redux';
+import { DeleteFormById, getFormById } from '../../actions/formActions';
+import { useHistory } from 'react-router';
+import formServices from '../../services/formServices';
 import pdfMake from 'pdfmake/build/pdfmake.js';
 import pdfFonts from 'pdfmake/build/vfs_fonts.js';
-import htmlToPdfmake from 'html-to-pdfmake'
-import Form from './Form'
-import { useTranslation } from 'react-i18next'
-import ReactDOMServer from "react-dom/server";
-import { setAlert } from '../../actions/alertActions'
-import { fetchFormList } from '../../actions/formListActions'
-import { SearchIcon } from '@material-ui/data-grid'
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
-
+import htmlToPdfmake from 'html-to-pdfmake';
+import Form from './Form';
+import { useTranslation } from 'react-i18next';
+import ReactDOMServer from 'react-dom/server';
+import { setAlert } from '../../actions/alertActions';
+import { fetchFormList } from '../../actions/formListActions';
+import { SearchIcon } from '@material-ui/data-grid';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 /**
  * @component
  * @desc A table to get and search for my forms.
  */
 const MyFormsTable: React.FC<any> = () => {
+  const myForms: any = useSelector((state: any) => state.formList.myForms);
 
-  const  myForms:any  = useSelector((state: any) => state.formList.myForms)
+  const [filter, setFilter] = React.useState('');
 
-  const [filter, setFilter] = React.useState('')
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const history = useHistory()
+  const history = useHistory();
 
   const classes = useStyles();
 
-  const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down('sm')) // sm: korkeintaan 960px
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm')); // sm: korkeintaan 960px
 
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   useEffect(() => {
-    dispatch(fetchFormList())
-  }, [dispatch])
+    dispatch(fetchFormList());
+  }, [dispatch]);
 
   // handle user input in the search field
-  const handleFilterchange = (event:any) => {
-    setFilter(event.target.value)
-
-  }
+  const handleFilterchange = (event: any) => {
+    setFilter(event.target.value);
+  };
 
   const handleEdit = (formId: any) => {
-    dispatch(getFormById(formId))
-    history.push(`/forms/edit-form`)
-  }
+    dispatch(getFormById(formId));
+    history.push(`/forms/edit-form`);
+  };
 
   const handleDelete = (formId: any) => {
-    dispatch(DeleteFormById(formId))
-    dispatch(setAlert("Form deleted successfully!"))
-  }
+    dispatch(DeleteFormById(formId));
+    dispatch(setAlert('Form deleted successfully!'));
+  };
 
   const handleDownload = async (formId: any) => {
-
-    let form:any = await formServices.fetchFormById(formId)
-    console.log("form ", form)
+    let form: any = await formServices.fetchFormById(formId);
+    console.log('form ', form);
 
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
     // pdf content
-    let content:any = []
+    let content: any = [];
 
-    let html = ReactDOMServer.renderToString(<Form currentForm={form}/>)
-    let htmlForm:any = htmlToPdfmake(html);
+    let html = ReactDOMServer.renderToString(<Form currentForm={form} />);
+    let htmlForm: any = htmlToPdfmake(html);
 
-    content.push(htmlForm)
+    content.push(htmlForm);
 
     // pdf document
     var doc = {
-      content: content
+      content: content,
     };
 
     pdfMake.createPdf(doc).download(form.title);
-
-  }
+  };
 
   // Table head styles
   const StyledTableCell = withStyles((theme: Theme) =>
-  createStyles({
-    head: {
-      backgroundColor: '#CCCCCC',
-      color: '#212121',
-    }
-  }),
+    createStyles({
+      head: {
+        color: '#EB5A02',
+      },
+    })
   )(TableCell);
-
 
   // Table row styles
   const StyledTableRow = withStyles((theme: Theme) =>
@@ -134,128 +126,144 @@ const MyFormsTable: React.FC<any> = () => {
           backgroundColor: theme.palette.action.hover,
         },
       },
-    }),
+    })
   )(TableRow);
-
 
   // Table view for desktop devices
   const tableView = () => {
     return (
-    <TableContainer style={{overflow:'auto'}}>
-      <Table aria-label="searched workers">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="left">{t("title")}</StyledTableCell>
-            <StyledTableCell align="left">{t("description")}</StyledTableCell>
-            <StyledTableCell align="left">{t("edit")}</StyledTableCell>
-            <StyledTableCell align="left">{t("delete")}</StyledTableCell>
-            <StyledTableCell align="left">{t("download")}</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {myForms.docs && myForms.docs.filter((form: any) => (
-            form.title.toLowerCase().includes(filter.toLowerCase())
-          )).map((form: any) => (
-            <StyledTableRow key={form._id}>
-            <TableCell align="left">{form.title}</TableCell>
-            <TableCell align="left">{form.description}</TableCell>
+      <TableContainer style={{ overflow: 'auto' }}>
+        <Table aria-label="searched workers">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">{t('title')}</StyledTableCell>
+              <StyledTableCell align="left">{t('description')}</StyledTableCell>
+              <StyledTableCell align="left">{t('edit')}</StyledTableCell>
+              <StyledTableCell align="left">{t('delete')}</StyledTableCell>
+              <StyledTableCell align="left">{t('download')}</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {myForms.docs &&
+              myForms.docs
+                .filter((form: any) =>
+                  form.title.toLowerCase().includes(filter.toLowerCase())
+                )
+                .map((form: any) => (
+                  <StyledTableRow key={form._id}>
+                    <TableCell align="left">{form.title}</TableCell>
+                    <TableCell align="left">{form.description}</TableCell>
 
-            <TableCell padding='none' align="center">
-              <IconButton aria-label="add to favorites" onClick={() => handleEdit(form._id)}>
-                  <EditIcon />
-              </IconButton>
-            </TableCell>
+                    <TableCell padding="none" align="center">
+                      <IconButton
+                        aria-label="add to favorites"
+                        onClick={() => handleEdit(form._id)}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </TableCell>
 
-            <TableCell padding="none" align="center">
-              <IconButton aria-label="share" onClick={() => handleDelete(form._id)}>
-                  <DeleteIcon />
-              </IconButton>
-            </TableCell>
+                    <TableCell padding="none" align="center">
+                      <IconButton
+                        aria-label="share"
+                        onClick={() => handleDelete(form._id)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
 
-            <TableCell padding="none" align="center">
-              <IconButton aria-label="share" onClick={() => handleDownload(form._id)}>
-                  <SaveAltIcon />
-              </IconButton>
-            </TableCell>
-
-          </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-
-    )
-  }
+                    <TableCell padding="none" align="center">
+                      <IconButton
+                        aria-label="share"
+                        onClick={() => handleDownload(form._id)}
+                      >
+                        <SaveAltIcon />
+                      </IconButton>
+                    </TableCell>
+                  </StyledTableRow>
+                ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
 
   // Accordion view for mobile devices
   const accordionView = () => {
-
     return (
-      myForms.docs && myForms.docs.filter((form: any) => (
-        form.title.toLowerCase().includes(filter.toLowerCase())
-      )).map((form: any) => (
-        <div key={form._id} className={classes.accordionDiv}>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>{form.title}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className={classes.description}>{form.description}</Typography>
-            </AccordionDetails>
-            <AccordionActions>
+      myForms.docs &&
+      myForms.docs
+        .filter((form: any) =>
+          form.title.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((form: any) => (
+          <div key={form._id} className={classes.accordionDiv}>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {form.title}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className={classes.description}>
+                  {form.description}
+                </Typography>
+              </AccordionDetails>
+              <AccordionActions>
+                <Tooltip title="Edit form" placement="top" arrow>
+                  <IconButton onClick={() => handleEdit(form._id)}>
+                    <EditIcon />
+                  </IconButton>
+                </Tooltip>
 
-              <Tooltip title="Edit form" placement="top" arrow>
-                <IconButton onClick={() => handleEdit(form._id)}>
-                  <EditIcon />
-                </IconButton>
-              </Tooltip>
+                <Tooltip title="Delete form" placement="top" arrow>
+                  <IconButton onClick={() => handleDelete(form._id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
 
-              <Tooltip title="Delete form" placement="top" arrow>
-                <IconButton onClick={() => handleDelete(form._id)}>
-                  <DeleteIcon />
-                </IconButton>
-              </Tooltip>
+                <Tooltip title="Download pdf" placement="top" arrow>
+                  <IconButton onClick={() => handleDownload(form._id)}>
+                    <SaveAltIcon />
+                  </IconButton>
+                </Tooltip>
+              </AccordionActions>
+            </Accordion>
+          </div>
+        ))
+    );
+  };
 
-              <Tooltip title="Download pdf" placement="top" arrow>
-                <IconButton onClick={() => handleDownload(form._id)}>
-                  <SaveAltIcon />
-                </IconButton>
-              </Tooltip>
-
-            </AccordionActions>
-          </Accordion>
-        </div>
-
-    ))
-
-    )
-
-  }
-
-  if(!myForms.docs) return (
-    <Typography style={{ padding: '1rem' }} variant="h6" align="center" className="text-secondary">
-      {t("no_results")}
-    </Typography>
-  )
+  if (!myForms.docs)
+    return (
+      <Typography
+        style={{ padding: '1rem' }}
+        variant="h6"
+        align="center"
+        className="text-secondary"
+      >
+        {t('no_results')}
+      </Typography>
+    );
 
   return (
     <div>
       <Box
-          display="flex"
-          justifyContent="flex-start"
-          alignItems="center"
-          flexWrap="wrap"
-        >
+        display="flex"
+        justifyContent="flex-start"
+        alignItems="center"
+        flexWrap="wrap"
+      >
         <form>
           <Box display="flex" alignItems="center">
             <InputBase
-                placeholder="Search by title..."
-                value={filter}
-                onChange={handleFilterchange}
+              placeholder="Search by title..."
+              value={filter}
+              onChange={handleFilterchange}
             />
             <IconButton>
               <SearchIcon />
@@ -266,8 +274,8 @@ const MyFormsTable: React.FC<any> = () => {
       <Divider />
       {matches ? accordionView() : tableView()}
     </div>
-  )
-}
+  );
+};
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -275,7 +283,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: '100%',
       marginTop: 12,
       border: '1px solid #E0E0E0',
-      borderRadius: 5
+      borderRadius: 5,
     },
     heading: {
       fontSize: theme.typography.pxToRem(14),
@@ -283,9 +291,9 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     description: {
       fontSize: theme.typography.pxToRem(13),
-      color: '#6C6C6C'
+      color: '#6C6C6C',
     },
-  }),
+  })
 );
 
-export default MyFormsTable
+export default MyFormsTable;
