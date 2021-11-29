9cb96b2c508ef41df5c74b82eb69d61ca442f362
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-11-29 13:34:43 +0200

    updated

--- src/pages/AgencyWorkRequest/ReceivedRequest.tsx
+++ src/pages/AgencyWorkRequest/ReceivedRequest.tsx
@@ -2,22 +2,30 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-} from "@material-ui/core"
-import React from "react"
+} from '@material-ui/core';
+import Typography from 'material-ui/styles/typography';
+import React from 'react';
 
 const ReceivedRequest: React.FC<any> = ({ workContracts }) => {
-  if (workContracts.docs === undefined) return <div> no results </div>
+  if (workContracts.docs === undefined) return <div> no results </div>;
 
   return (
     <div>
       {workContracts?.docs.map((workContract: any) => (
         <Accordion key={workContract._id}>
           <AccordionSummary>aaa</AccordionSummary>
-          <AccordionDetails>{workContract.docs}</AccordionDetails>
+          <AccordionDetails>
+            {workContract?.contracts?.map((contract: any) => (
+              <div key={contract._id}>
+                <p>worker count {contract.workerCount}</p>
+                <p>Gig details {contract.detailedInfo}</p>
+              </div>
+            ))}
+          </AccordionDetails>
         </Accordion>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ReceivedRequest
+export default ReceivedRequest;
