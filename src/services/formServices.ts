184cca0891feaea11216f74a4da94363710715f6
Amal Kayed <amal.kayed88@gmail.com>
2022-06-05 12:43:12 +0300

    Changed form2 name to form

--- src/services/formServices.ts
+++ src/services/formServices.ts
@@ -2,63 +2,63 @@
  * @module service/feeling
  * @desc Feeling requests to backend.
  */
- import axios from 'axios'
- import { loadUser } from '../utils/storage'
- import baseUrl from '../utils/baseUrl'
+import axios from "axios";
+import { loadUser } from "../utils/storage";
+import baseUrl from "../utils/baseUrl";
 
- /**
-  * @function
-  * @desc Helper function for setting up request header.
-  */
- const authHeader = () => {
-   return {
-     headers: { 'x-access-token': `${loadUser().token}` }
-   }
- }
+/**
+ * @function
+ * @desc Helper function for setting up request header.
+ */
+const authHeader = () => {
+  return {
+    headers: { "x-access-token": `${loadUser().token}` },
+  };
+};
 
- /**
-  * @function
-  * @desc Fetches myForms
-  */
- const fetchMyFormList = async () => {
-   try{
-    const res = await axios.get(`${baseUrl}/form2/myForm`, authHeader())
-    return res.data
-   } catch(error){
+/**
+ * @function
+ * @desc Fetches myForms
+ */
+const fetchMyFormList = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}/form/myForm`, authHeader());
+    return res.data;
+  } catch (error) {
     console.log(error);
-    return {docs: []}
-   }
- }
+    return { docs: [] };
+  }
+};
 
- const fetchCommunityFormList = async () => {
-   try {
-    const res = await axios.get(`${baseUrl}/form2/public`, authHeader())
-    return res.data
-   } catch (error) {
+const fetchCommunityFormList = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}/form/public`, authHeader());
+    return res.data;
+  } catch (error) {
     console.log(error);
-    return {docs: []}
-   }
-}
+    return { docs: [] };
+  }
+};
 
 const fetchCommonFormList = async () => {
   try {
-   const res = await axios.get(`${baseUrl}/form2/common`, authHeader())
-   return res.data
+    const res = await axios.get(`${baseUrl}/form/common`, authHeader());
+    return res.data;
   } catch (error) {
-   console.log(error);
-   return {docs: []}
+    console.log(error);
+    return { docs: [] };
   }
-}
+};
 
 const fetchFormById = async (id: string) => {
   try {
-   const res = await axios.get(`${baseUrl}/form2/myForm/${id}`, authHeader())
-   return res.data
+    const res = await axios.get(`${baseUrl}/form/myForm/${id}`, authHeader());
+    return res.data;
   } catch (error) {
-   console.log(error);
-   return {}
+    console.log(error);
+    return {};
   }
-}
+};
 
 /**
  * @todo make converted form type.
@@ -68,49 +68,56 @@ const fetchFormById = async (id: string) => {
 const postForm = async (form: any) => {
   try {
     console.log("formServices:form: ", form);
-    const res = await axios.post(`${baseUrl}/form2/`, form, authHeader())
-    return res
+    const res = await axios.post(`${baseUrl}/form/`, form, authHeader());
+    return res;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 /**
  * @todo make converted form type.
  * @param form
  * @returns
  */
- const updateForm = async (formId:any, form: any) => {
+const updateForm = async (formId: any, form: any) => {
   try {
     console.log("formService:updateForm: formId ", formId);
     console.log("formService:updateForm: FormObject ", form);
-    const res = await axios.put(`${baseUrl}/form2/update/${formId}`, form, authHeader())
-    return res
+    const res = await axios.put(
+      `${baseUrl}/form/update/${formId}`,
+      form,
+      authHeader()
+    );
+    return res;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 /**
  * @param id
  * @returns
  */
- const deleteForm = async (formId:any) => {
+const deleteForm = async (formId: any) => {
   try {
-    const res = await axios.delete(`${baseUrl}/form2/delete/${formId}`, authHeader())
-    console.log("delete res", res)
-    return res
+    const res = await axios.delete(
+      `${baseUrl}/form/delete/${formId}`,
+      authHeader()
+    );
+    console.log("delete res", res);
+    return res;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
- export default {
+export default {
   fetchMyFormList,
   fetchCommunityFormList,
   fetchCommonFormList,
   fetchFormById,
   postForm,
   updateForm,
-  deleteForm
- }
+  deleteForm,
+};
