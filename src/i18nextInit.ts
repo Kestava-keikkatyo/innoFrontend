fb65d2eee5762b79465a6a661f970f03553e751b
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2022-06-07 17:05:25 +0300

    New branch for French language version started

--- src/i18nextInit.ts
+++ src/i18nextInit.ts
@@ -4,9 +4,10 @@ import Backend from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import translationFI from './translations/fi'
 import translationEN from './translations/en'
+import translationFR from './translations/fr'
 
 const fallbackLng = ['en']
-const availableLanguages = ['fi', 'en']
+const availableLanguages = ['fi', 'en', 'fr']
 
 const resources = {
   fi: {
@@ -14,7 +15,10 @@ const resources = {
   },
   en: {
     translation: translationEN
-  }
+  },
+  fr: {
+    translation: translationFR
+  },
 }
 
 i18n
---
+++ src/translations/fr.ts
@@ -0,0 +1,637 @@
+export default {
+  //home page
+  information: "Information",
+  how_do_you_feel_today: "How do you feel today?",
+  thank_you: "Feeling received!",
+  home: "Home",
+  thank_you_for_feedback: "Thank you for your feedback!",
+  write_a_comment: "Write a comment",
+  workers_responsibility: "Worker's responsibility",
+  worker_responsibilities_read: "Worker has read and understood his/her responsibilities.",
+  agency_responsibility: "Agency's responsibility",
+  agency_responsibilities_read:
+    "Agency has read and understood its responsibilities.",
+  business_responsibility: "Business' responsibility",
+  business_responsibilities_read:
+    "Business has read and understood its responsibilities.",
+  saveButton: "Save",
+  faq: "Frequently asked questions",
+
+  //login
+  login_title: "Log In",
+  email_label: "Email",
+  password_label: "Password",
+  invalid_email_address: "Invalid email address",
+  required: "Required",
+  invalid_token: "Invalid token",
+  logged_out: "Logged out",
+  login_failed: "Login failed",
+  login_successful: "Login success",
+
+  //signup
+  name: "Name",
+  email: "Email",
+  password: "Password",
+  confirm: "Confirm password",
+  user: "User",
+  terms_of_use: "By registering, you agree to give your information to ",
+  terms_agency: "agencies",
+  signup_successful: "Signup successful",
+
+  //error
+  invalid_email: "Invalid email address",
+  invalid_length: "Length must be at least 3",
+  field_required: "Field is required",
+  invalid_name: "Invalid name",
+  invalid_password: "Password does not match",
+  email_already_used: "Email is already in use",
+
+  //option
+  role: "Role",
+  worker: "Worker",
+  agency: "Agency",
+  business: "Business",
+  jobs: "Jobs",
+  businesses: "Businesses",
+  workers: "Workers",
+
+  //Button
+  send: "Send",
+  submit: "Submit",
+  log_in: "Log in",
+  sign_up: "Sign up",
+  main_page: "Go to main page",
+  back: "back",
+  next: "next",
+  finish: "Send",
+  reset: "Reset",
+  upload_file: "Upload file",
+  entry: "New entry",
+  history: "History",
+  active: "Active",
+  create: "Create",
+  button_update: "update",
+  button_edit: "edit",
+
+  //Mood index page
+  entry_mood: "Add mood",
+
+  //profile page
+  profiles: "Profiles",
+  search_by_name: "Search by name...",
+  no_results: "No results",
+  search: "Search...",
+
+  //Line chart
+  mood_history: "Mood history",
+
+  //mood_category
+  mood_category: "Mood Category",
+
+  //Pie chaert
+  mood_overall: "Mood overall",
+  worst: "Very Dissatisfied",
+  bad: "Dissatisfied",
+  neutral: "Neutral",
+  good: "Satisfied",
+  excellent: "Very Satisfied",
+
+  mood_table: "Details",
+  mood: "Feeling",
+  moods: "Feelings",
+  comment: "Comment",
+  date: "Date",
+  image: "Image",
+
+  //Agencystaistics
+  mood_stats: "Feeling stats",
+
+  //Report form
+  report_handler: "Reports",
+  report_time: "Report time",
+  report_details: "Details",
+  steps_completed: "Steps are completed - you are finished",
+  reports: "Reports",
+  report: "Report",
+  report_no_recipient: "You need to select report handler.",
+  report_title_required: "Report title is required",
+  report_details_required: "Report details are required",
+  report_new_report: 'New Report',
+  report_back_to_reports: 'Back to reports',
+
+  //Report step one
+  select_report_handler: "Select report handler",
+  select_report_helper_text:"You can choose both Business and Agency or one of them as a report handler",
+  select_report_handler_clear: "Clear",
+  //Report step two
+  happened: "When did this happen?",
+  date_picker_inline: "Date",
+  time_picker: "Time",
+  report_invalid_date_alert: 'Please check date and time.',
+  report_date_cannot_be_in_future: 'Date cannot be set in future',
+  report_time_cannot_be_in_future: 'Time cannot be set in future',
+
+  //Report step three
+  fill_details: "Fill in details",
+  fill_work_title: "Fill work title",
+  fill_report_title: "Fill report title",
+  fill_report_details: "Fill report details",
+
+  //Reports page
+  reports_not_found: "No reports found.",
+  reports_you_have_no_reports: 'You have not made any reports.',
+  report_title: 'Report title',
+  report_worker_info: 'Worker info',
+  report_receiver_info: 'Recipient info',
+  report_receiver_info_plural: 'Recipients info',
+  report_answer_button: 'Reply',
+  report_status_pending: 'Waiting for reply',
+  report_status_replied: 'Replied',
+  report_status_partially_replied: "Partially replied",
+  report_from_agency: 'from Agency',
+  report_from_business: 'from Business',
+  report_display_all: 'All',
+  report_display_archived: 'Archived',
+  report_display_not_archived: 'Not archived',
+  report_display_label: 'Display',
+  report_sort_label: 'Sort',
+  report_sort_date_created_new_to_old: 'By reporting date',
+  report_sort_date_created_new_to_old_reverse: 'By reporting date, reversed',
+  report_sort_date_happened_new_to_old: 'By date happened',
+  report_sort_date_happened_new_to_old_reverse: 'By date happened, reversed',
+  report_sort_title: 'By title',
+  report_sort_title_reverse: 'By title, reversed',
+  report_sort_replied: 'By replied',
+  report_sort_replied_reverse: 'By replied, reversed',
+  report_is_archived: 'Archived',
+  report_is_archived_alert: 'Report archived',
+  report_is_unarchived_alert: 'Report unarchived',
+  report_archive: 'Archive',
+  report_unarchive: 'Unarchive',
+  report_time_reported: 'Time when reported',
+  report_time_happened: 'Time when happened',
+
+  //ReportReply page
+  report_reply_title: 'Reply to report',
+  report_reply_answer: 'Reply',
+  report_reply_send: 'Send',
+  report_reply_sent_alert: 'Reply sent',
+  report_reply_reply_missing: 'Reply missing!',
+  report_reply_you_have_new_reply_in_report: 'You have a new reply in report:',
+
+  //Mood form
+  your_mood: "Your mood",
+
+  //Mood step three
+  write_comment: "Write a comment",
+  tell_feelings: "Tell us about your feeling...",
+
+  //BusinessContractsPage
+  search_agencies: "Search agencies",
+  sent_contracts: "Sent contracts",
+  received_contracts: "Received contracts",
+  waiting_contracts: "Waiting contracts",
+  done_contracts: "Done contracts",
+  contracts_received_from_the_workers: "Contracts received from the workers",
+  contracts_received_from_businesses: "Contracts received from the businesses",
+
+  //Agencies list
+  all: "All",
+  construction: "Construction",
+  it: "IT",
+  education: "Education",
+  medicine: "Pharmaceutical industry",
+  estate: "Estate",
+  transfer_profile: "Go to profile",
+  transfer_company_profile: "Go to company profile",
+
+  //Contact information
+  phone: "Phone",
+  website: "Website",
+  address: "Address",
+  contact_information: "Contact information",
+
+  //feeling utils
+  cheer_one: "C'moon!",
+  cheer_two: "Not Bad!",
+  cheer_three: "Decent!",
+  cheer_four: "Great!",
+  cheer_five: "Excellent!",
+
+  //Feeling entryModal
+  feeling_entry: "New Feeling Entry",
+
+  //AgencyStatisticsSummary
+  average: "Average",
+  total: "Total",
+
+  //WorkerStatisticsSummary,
+  current_mood: "Current mood",
+  no_data: "Oops! No data.",
+  no_entries:
+    "If you see this, then you haven't probably added any entries yet.",
+  oops: "oops",
+
+  //WorkerTransferList
+  choices: "choices",
+  chosen: "chosen",
+
+  //Contracts pages
+  received_work_requests: "Received task requests",
+  accepted_work_requests: "Accepted task requests",
+  neededWork: "Requested task",
+  activeWork: "Active tasks",
+  endedWork: "Ended tasks",
+
+  //ContractsPage
+  own_contracts: "Own contracts",
+  arrived_contracts: "Arrived contracts",
+  requests_of_contracts: "Requests of contracts",
+  add_active: "Add to active work tasks",
+  add: "Add",
+  make_contract: "Make a contract",
+  contracts: "Contracts",
+  contracts_overview: "Contracts overview",
+
+  //FeedbackForm
+  write_feedback_here: "Write feedback here",
+  feedback_sent: "Feedback sent",
+  give_feedback:
+    "This component is temporary, it is only used during application development. The aim is to get feedback and development ideas from the project team and testers, which will be taken into account during application development.",
+  return_form: "Feedback form",
+  replied: "Replied",
+  wait: "Wait",
+  own_feedback: "Own feedbacks",
+  headline: "Headline",
+
+  //ContractFormManagerPage
+  description: "Description",
+  preview: "Preview",
+  edit: "Edit",
+  download_pdf: "Download pdf",
+  download: "Download",
+  copy_to_my_forms: "Copy to my forms",
+
+  //MyFormsTable
+  delete: "Delete",
+
+  //FormsPage
+  my_forms: "My forms",
+  common: "Common",
+  community: "Community",
+  common_forms: "Common forms",
+  community_forms: "Community forms",
+  search_by_title: "Search by title",
+
+  //Agency
+  username: "username",
+
+  //ContractsTable
+  type: "Type",
+  status: "Status",
+  accept: "Accept",
+  info: "Info",
+  send_back: "Send back",
+  decline: "Decline",
+
+  //ResponsiveDrawer
+  databank: "Databank",
+  schedule: "Schedule",
+  work_overview: "Work overview",
+  forms: "Forms",
+  work_request: "Work requests",
+  exit_application: "Exit application",
+  business_contracts: "Business contracts",
+  my_work: "My work",
+  remove: "Remove",
+  close: "close",
+  feedback: "Feedback",
+
+  //profilePage
+  edit_profile: "Edit profile",
+  settings: "Settings",
+  profile: "Profile",
+  logout: "Logout",
+  return: "Return",
+  introduction_video: "Introduction video",
+  instructions: "Instructions",
+  occupational: "Occupational Safety Rules",
+  change_picture: "Change picture",
+  change_cover: "Change cover",
+  change_introduction_video: "Change introduction video",
+  save_changes: "Save changes",
+
+  //ChangePassword
+  change_password: "Change password",
+  update_password: "Update password",
+  password_instructions:
+    " Enter your current password, the new password and then confirm the new password",
+  current_password: "Current password",
+  new_password: "New password",
+  confirm_password: "Confirm new password",
+  password_update_succesful: "Password updated successfully!",
+
+  //FeedbackForm
+  no_feedback: "No feedback.",
+
+  //weekdays
+  monday: "Mon",
+  tuesday: "Tue",
+  wednesday: "Wed",
+  thursday: "Thu",
+  friday: "Fri",
+
+  w: "1W",
+  mm: "1M",
+  half_year: "6M",
+  year: "1Y",
+
+  jan: "January",
+  feb: "February",
+  mar: "March",
+  apr: "April",
+  may: "May",
+  jun: "June",
+  jul: "July",
+  aug: "August",
+  sep: "September",
+  oct: "October",
+  nov: "November",
+  dec: "December",
+
+  //ListAccordion
+  unfinished: "Unfinished",
+
+  //Form
+  form_title: "Form title",
+  add_question: "Add question",
+  form_editor: "Form editor",
+  form_tooltip_preview: 'Preview form',
+  form_tooltip_edit: 'Edit form',
+  form_tooltip_delete: 'Delete form',
+  form_tooltip_download: 'Download form',
+  form_tooltip_copy: 'Copy to my forms',
+
+  finland: "Finnish",
+  english: "English",
+  tyopoyta: "Bureau",
+  tietopankki: "Banque informatique",
+  kirjaudu_sisaan: "Se connecter",
+  //Joblist
+  limit_search: "Limit search",
+  jobs_found: "Jobs found",
+  participants: "Participants",
+  company_sites: "Company websites",
+  enroll: "Enroll ",
+  relocation: "relocation",
+  accept_terms: "I accept the terms",
+
+  //jobmodal
+  send_agency_contract: " Send work request to agency",
+  choose_agency: "Choose agency",
+  tell_position: " Tell headline / position",
+  worker_count: "Worker count",
+
+  //PendingJobPosts
+  pending_jobs: "Pending jobs",
+  work_task: "Work task",
+  workers_count: "Workers count",
+  start_date: "Start date",
+  end_date: "End date",
+  created: "Created at",
+  joined: "Joined at",
+  agency_status: "Agency status",
+  business_status: "Business status",
+
+  //feedbackCategory
+  feedback_category: "Feedback category",
+  category0: "Category",
+  description0:
+    "Description : based on the amount of mood feedback and the mood points",
+  description1: "According to our statistics you could use a little cheering.",
+  description2: "According to our statistics your mood is quite well.",
+  description3: "According to our statistics you are feeling well.",
+  description4: "According to our statistics your mood is really good.",
+  description5: "According to our statistics your mood is very good.",
+
+  //contractsFromBusiness
+  contracts_from_business: "Contracts from business",
+
+  choose_request: "Choose request",
+
+  business_website: "Business website",
+
+  contract_form: "Contract form",
+
+  delete_option: "Delete option",
+  add_an_option: "Add an option",
+
+  create_new_form: "Create new form",
+  create_contract: "Create contract",
+
+  when_did_this_happen: "When did this happen?",
+
+  read_more: "Read more",
+
+  fill_business_contract_form: "Fill business contract form",
+  preview_business_contract_form: "Preview business contract form",
+
+  choose_workers: "Choose the Workers",
+  agency_description_here: "agency_description_here",
+
+  edit_filled: "Edit Filled Business Contract",
+
+  cooperation_send: "Send cooperation request",
+  send_contract: "Send contract",
+
+  edit_contract_form: "Edit Contract Form",
+  contact_form_manager: "Contract form manager",
+
+  form_preview: "Form preview",
+  your_title: "Title",
+  your_description: "Description",
+  create_job: "Create job",
+
+  choose: "Choose",
+
+  category: "Category",
+  details: "Details",
+  requirements: "Requirements",
+  duration: "Duration",
+  title: "Title",
+  add_job: "Add job",
+  job_start_date: "Start date",
+  job_end_date: "End date",
+  application_ends: "Application ends",
+  street_address: "Street address",
+  city: "City",
+  zip_code: "Zip code",
+  add_job_button: "Add",
+
+  //WorkdView
+  contract: "Contract",
+  contacts: "Contact",
+
+  //LimitJobSearch
+  locations: "Locations",
+  scope: "Scope",
+  experience: "Experience",
+
+  //JobsIncidence
+  newest: "Newest",
+  oldest: "Oldest",
+  closing_soon: "Closing soon",
+  select_by_incidence: "Select by incidence",
+
+  //cooperation info modal
+  send_cooperation_request: "Send co-operation request",
+  agency_info: "Agency info",
+  agency_name: "Name",
+  agency_category: "Category",
+  agency_email: "Email",
+  select_contract_form: "Select contract form",
+  selected_form: "Selected form",
+  none: "None",
+
+  //Notifications
+  notifications: "Notifications",
+  no_notifications: "No notifications...",
+  empty_notifications: "Empty all notifications",
+
+  //DatabankRoute
+  homeButton: "Home",
+  articles: "Articles",
+  work_lifespan: "Work lifespan",
+  areas_of_responsibility: "Areas of responsibility",
+  good_practises: "Good practises",
+
+  //NewFormHeader
+  import: "Import",
+  export: "Export",
+
+  //QuestionModule
+  question_type: "Question type",
+  question_module: "Question:",
+  your_question: "Your question",
+
+  //ExpandableQuestionOptions
+  type_subtitle_here: "Type subtitle here...",
+  question_sub_title: "Question sub title",
+  answer_min_len: "Answer min len",
+  answer_max_len: "Answer max len",
+  scale: "Scale",
+  scale_option_title_left: "scale option title left",
+  left_title: "Left title",
+  scale_option_title_center: "scale option title center",
+  center_title: "Center title",
+  scale_option_title_right: "scale option title right",
+  right_title: "Right title",
+  row_height: "row height",
+  is_answer_optional: "is answer optional",
+  expand: "Expand",
+  closing: "Closing",
+  services: "Services",
+  care: "Care",
+  marketing_sales: "Marketing and sales",
+  trainee: "Trainee",
+  experienced: "Experienced",
+  professional: "Professional",
+
+  user_information_updated: "User information updated",
+
+  //FAQ
+  show_less: "Show less",
+  show_more: "Show more",
+
+  //landingPage
+  slogan: "Quand le personnel se sent bien, le travail s'écoule.",
+  intro:
+    "Keikkakaveri fournit des informations, des formations et des équipements pour développer et maintenir des conditions de travail sûres et saines.",
+
+  //CreateJobForAgency
+  add_new_job: "Add new job",
+  job_title: "Title",
+  job_category: "Category",
+  job_type: "Job type",
+  job_salary: "Salary",
+  job_street: "Street",
+  job_zipCode: "Zip code",
+  job_city: "City",
+  job_applicationLastDate: "Application last date",
+  job_startDate: "Start date",
+  job_endDate: " End date",
+  job_requirements: "Requirements",
+  job_desirableSkills: "Desirable skills",
+  job_benefits: "Benefits",
+  job_details: "Details",
+  job_created_successfully: "Job created successfully!",
+  failed_to_create_the_job: "Failed to create the job!",
+  job_details_title: "Job Details",
+  job_supplier: "Supplier",
+  job_specifics: "Specifics",
+  job_posted_at: "Posted at",
+  job_available_until: "Available untill",
+  job_read_more: "Read more",
+  job_release_date: "Release Date",
+  job_update: "Update",
+  job_action: "Action",
+  requirements_and_responsibilities: "Requirements and Responsibilities",
+  full_job_description: "Full description",
+  job_ads: "Job ads",
+  job_apply: "Apply now",
+  job_your_job_ads: 'Your job ads',
+
+  //CreateUser
+  add_new_admin: "Add new admin",
+  create_user_name: "Name",
+  create_user_email: "Email",
+  create_user_password: "Password",
+  create_user_confirm_password: "Confirm Password",
+  user_created_successfully: "User created successfully!",
+  name_should_be_three_letters_at_least:
+    "Name should be three letters at least!",
+  name_is_required: "Name is required!",
+  not_valid_email: "Not valid email!",
+  email_is_required: "Email is required!",
+  password_is_required: "Password is required!",
+  passwords_must_match: "Passwords must match!",
+
+  //List
+  list_title_workers: "Workers",
+  list_title_agencies: "Agencies",
+  list_name: "Name",
+  list_email: "Email",
+  list_city: "City",
+  list_position: "Position",
+  list_action: "Action",
+  list_profile: "Profile",
+  list_category: "Category",
+
+  //User profile
+  user_profile: "Profile",
+  user_account_details: "Account Details",
+  user_contact_details: "Contact Details",
+  user_licenses: "Licenses",
+  user_edit_profile: "Edit profile",
+  user_name: "Name",
+  user_email: "Email",
+  user_city: "City",
+  user_zipCode: "Zip code",
+  user_street: "Street",
+  user_phone_number: "Phone number",
+  user_website: "Website",
+  user_category: "Category",
+  user_updated_successfully: "User was updated successfully!",
+
+  //user feedback
+  send_new_feedback: "Send new feedback",
+  your_feedbacks: "Your feedbacks",
+  send_feedback: "Send feedback",
+  feedback_title: "Title",
+  feedback_message: "Message",
+  feedback_recipient: "Recipient",
+  sending_date: "Sending date",
+  feedback_action: "Action",
+  feedback_details: "details",
+  feedback_title_details: "Feedback details",
+};
