c914b56f12354f317cbf73bdbc4bd7925bb252d7
Amal Kayed <amal.kayed88@gmail.com>
2022-06-19 20:51:44 +0300

    Updated fileService and fileReducer

--- src/reducers/fileReducer.ts
+++ src/reducers/fileReducer.ts
@@ -1,38 +1,29 @@
-
-
 /**
  * @module reducer/file
  * @desc Redux file reducer
  */
- import { SET_CURRENT_FILES, FileState, FileActionTypes} from "../types/state"
- import {File} from "../types/types"
+import { SET_CURRENT_FILES, FileState, FileActionTypes } from '../types/state'
 
- const initialFiles: File = {
-    files: [null,null,null]
+const initialState: FileState = {
+  currentFiles: [null, null, null],
+}
 
- }
- const initialState: FileState = {
-   currentFiles: initialFiles
- }
-
- /**
-  * @function
-  * @desc Redux file reducer that controls the file state.
-  * @param {FileState} state - initial state
-  * @param {A} action - dispatched action
-  */
- const fileReducer = (state: FileState = initialState, action: FileActionTypes) => {
-    switch (action.type) {
-      case SET_CURRENT_FILES:
-       return {
-         ...state,
-         currentFiles:{
-           files: action.data
-         }
-       }
-      default:
-        return state
-    }
+/**
+ * @function
+ * @desc Redux file reducer that controls the file state.
+ * @param {FileState} state - initial state
+ * @param {A} action - dispatched action
+ */
+const fileReducer = (state: FileState = initialState, action: FileActionTypes) => {
+  switch (action.type) {
+    case SET_CURRENT_FILES:
+      return {
+        ...state,
+        currentFiles: action.data,
+      }
+    default:
+      return state
   }
+}
 
- export default fileReducer
+export default fileReducer
