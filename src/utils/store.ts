446a579fc7f89849e6adfc4cdaa72c4525af7615
Amal Kayed <amal.kayed88@gmail.com>
2022-06-23 07:19:59 +0300

    Added notification reducer to store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,27 +1,28 @@
-import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import userReducer from "../reducers/userReducer";
-import fileReducer from "../reducers/fileReducer";
-import alertReducer from "../reducers/alertReducer";
-import businessContractReducer from "../reducers/businessContractReducer";
-import workContractReducer from "../reducers/workContractReducer";
-import breadcrumbReducer from "../reducers/breadcrumbReducer";
-import feelingReducer from "../reducers/feelingReducer";
-import formReducer from "../reducers/formReducer";
-import formListReducer from "../reducers/formListReducer";
-import buisnessContractFormReducer from "../reducers/businessContractFormReducer";
-import allUsersReducer from "../reducers/allUsersReducer";
-import profileReducer from "../reducers/profileReducer";
-import notificationsReducer from "../reducers/notificationsReducer";
-import reportReducer from "../reducers/reportReducer";
-import jobReducer from "../reducers/jobReducer";
-import usersReducer from "../reducers/usersReducer";
-import feedBackReducer from "../reducers/feedBackReducer";
-import topicReducer from "../reducers/topicReducer";
-import { LOGOUT } from "../types/state";
-import workRequestReducer from "../reducers/workRequestReducer";
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import userReducer from '../reducers/userReducer'
+import fileReducer from '../reducers/fileReducer'
+import alertReducer from '../reducers/alertReducer'
+import businessContractReducer from '../reducers/businessContractReducer'
+import workContractReducer from '../reducers/workContractReducer'
+import breadcrumbReducer from '../reducers/breadcrumbReducer'
+import feelingReducer from '../reducers/feelingReducer'
+import formReducer from '../reducers/formReducer'
+import formListReducer from '../reducers/formListReducer'
+import buisnessContractFormReducer from '../reducers/businessContractFormReducer'
+import allUsersReducer from '../reducers/allUsersReducer'
+import profileReducer from '../reducers/profileReducer'
+import notificationsReducer from '../reducers/notificationsReducer'
+import reportReducer from '../reducers/reportReducer'
+import jobReducer from '../reducers/jobReducer'
+import usersReducer from '../reducers/usersReducer'
+import feedBackReducer from '../reducers/feedBackReducer'
+import topicReducer from '../reducers/topicReducer'
+import { LOGOUT } from '../types/state'
+import workRequestReducer from '../reducers/workRequestReducer'
+import notificationReducer from '../reducers/notificationReducer'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const appReducer = combineReducers({
   user: userReducer,
@@ -43,19 +44,17 @@ const appReducer = combineReducers({
   users: usersReducer,
   topic: topicReducer,
   workRequest: workRequestReducer,
-});
+  notification: notificationReducer,
+})
 
 const rootReducer = (state: any, action: any) => {
   if (action.type === LOGOUT) {
-    return appReducer(undefined, action);
+    return appReducer(undefined, action)
   }
 
-  return appReducer(state, action);
-};
+  return appReducer(state, action)
+}
 
-export type IRootState = ReturnType<typeof rootReducer>;
+export type IRootState = ReturnType<typeof rootReducer>
 
-export default createStore<any, any, any, any>(
-  rootReducer,
-  composeEnhancer(applyMiddleware(thunk))
-);
+export default createStore<any, any, any, any>(rootReducer, composeEnhancer(applyMiddleware(thunk)))
