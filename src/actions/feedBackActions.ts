24a9d30cca4edef2c7717b377ba983f0373c281d
Amal Kayed <amal.kayed88@gmail.com>
2022-06-28 00:51:51 +0300

    Replaced any with the right types in fetch all feedbacks functions and formatted code

--- src/actions/feedBackActions.ts
+++ src/actions/feedBackActions.ts
@@ -1,47 +1,57 @@
-import feedBackService from "../services/feedBackService";
-import { Feedback, feedbackType, severity } from "../types/types";
-import { setAlert } from "./alertActions";
-import history from "../utils/history";
+import feedBackService from '../services/feedBackService'
+import { Feedback, feedbackType, severity } from '../types/types'
+import { setAlert } from './alertActions'
+import history from '../utils/history'
+import { Dispatch } from 'redux'
+import { FeedbackActionFailure, FeedbackGetAllRequest, FeedbackGetAllSuccess } from '../types/state'
 
 /**
  * @function
  * @desc Fetches all user's feedbacks.
  */
-export const fetchAllMyFeedbacks = () => async (dispatch: any) => {
-  try {
-    dispatch({
-      type: feedbackType.FEEDBACK_GET_ALL_REQUEST,
-    });
-    const res = await feedBackService.fetchAllMyFeedbacks();
-    dispatch({ type: feedbackType.FEEDBACK_GET_ALL_SUCCESS, data: res.data });
-  } catch (e) {
-    dispatch({
-      type: feedbackType.FEEDBACK_ACTION_FAILURE,
-      data: e,
-    });
-    dispatch(setAlert("Failed to fetch feedbacks!: " + e, severity.Error, 15));
+export const fetchAllMyFeedbacks =
+  () =>
+  async (
+    dispatch: Dispatch<FeedbackGetAllRequest | FeedbackGetAllSuccess | FeedbackActionFailure>,
+  ) => {
+    try {
+      dispatch({
+        type: feedbackType.FEEDBACK_GET_ALL_REQUEST,
+      })
+      const res = await feedBackService.fetchAllMyFeedbacks()
+      dispatch({ type: feedbackType.FEEDBACK_GET_ALL_SUCCESS, data: res.data })
+    } catch (e) {
+      dispatch({
+        type: feedbackType.FEEDBACK_ACTION_FAILURE,
+        data: e as string,
+      })
+      setAlert('Failed to fetch feedbacks!: ' + e, severity.Error, 15)(dispatch)
+    }
   }
-};
 
 /**
  * @function
  * @desc Fetches all feedbacks for admin.
  */
-export const fetchAllFeedbacksForAdmin = () => async (dispatch: any) => {
-  try {
-    dispatch({
-      type: feedbackType.FEEDBACK_GET_ALL_REQUEST,
-    });
-    const res = await feedBackService.fetchAllFeedbacksForAdmin();
-    dispatch({ type: feedbackType.FEEDBACK_GET_ALL_SUCCESS, data: res.data });
-  } catch (e) {
-    dispatch({
-      type: feedbackType.FEEDBACK_ACTION_FAILURE,
-      data: e,
-    });
-    dispatch(setAlert("Failed to fetch feedbacks!: " + e, severity.Error, 15));
+export const fetchAllFeedbacksForAdmin =
+  () =>
+  async (
+    dispatch: Dispatch<FeedbackGetAllRequest | FeedbackGetAllSuccess | FeedbackActionFailure>,
+  ) => {
+    try {
+      dispatch({
+        type: feedbackType.FEEDBACK_GET_ALL_REQUEST,
+      })
+      const res = await feedBackService.fetchAllFeedbacksForAdmin()
+      dispatch({ type: feedbackType.FEEDBACK_GET_ALL_SUCCESS, data: res.data })
+    } catch (e) {
+      dispatch({
+        type: feedbackType.FEEDBACK_ACTION_FAILURE,
+        data: e as string,
+      })
+      setAlert('Failed to fetch feedbacks!: ' + e, severity.Error, 15)(dispatch)
+    }
   }
-};
 
 /**
  * @function
@@ -51,22 +61,20 @@ export const fetchFeedbackById = (id: string) => async (dispatch: any) => {
   try {
     dispatch({
       type: feedbackType.FEEDBACK_GET_CURRENT_REQUEST,
-    });
-    const res = await feedBackService.fetchFeedbackById(id);
+    })
+    const res = await feedBackService.fetchFeedbackById(id)
     dispatch({
       type: feedbackType.FEEDBACK_GET_CURRENT_SUCCESS,
       data: res.data,
-    });
+    })
   } catch (error) {
     dispatch({
       type: feedbackType.FEEDBACK_ACTION_FAILURE,
       data: error,
-    });
-    dispatch(
-      setAlert("Failed to fetch the feedback: " + error, severity.Error, 15)
-    );
+    })
+    dispatch(setAlert('Failed to fetch the feedback: ' + error, severity.Error, 15))
   }
-};
+}
 
 /**
  * Send feedback
@@ -79,48 +87,45 @@ export const createFeedback = (feedback: Feedback) => async (dispatch: any) => {
     dispatch({
       type: feedbackType.FEEDBACK_POSTED_REQUEST,
       data: feedback,
-    });
+    })
 
-    const { data } = await feedBackService.createFeedback(feedback);
+    const { data } = await feedBackService.createFeedback(feedback)
     dispatch({
       type: feedbackType.FEEDBACK_POSTED_SUCCESS,
       data,
-    });
-    dispatch(setAlert("Feedback was sent successfully!"));
+    })
+    dispatch(setAlert('Feedback was sent successfully!'))
   } catch (e) {
     dispatch({
       type: feedbackType.FEEDBACK_ACTION_FAILURE,
       data: e,
-    });
-    dispatch(setAlert("Failed to send feedback: " + e, severity.Error, 15));
+    })
+    dispatch(setAlert('Failed to send feedback: ' + e, severity.Error, 15))
   }
-};
+}
 
 /**
  * @function
  * @desc update feedback.
  */
-export const updateFeedback =
-  (feedbackId: string, feedback: Feedback) => async (dispatch: any) => {
-    try {
-      dispatch({
-        type: feedbackType.FEEDBACK_UPDATED_REQUEST,
-      });
+export const updateFeedback = (feedbackId: string, feedback: Feedback) => async (dispatch: any) => {
+  try {
+    dispatch({
+      type: feedbackType.FEEDBACK_UPDATED_REQUEST,
+    })
 
-      const res = await feedBackService.updateFeedback(feedbackId, feedback);
-      dispatch({
-        type: feedbackType.FEEDBACK_UPDATED_SUCCESS,
-        data: res.data,
-      });
+    const res = await feedBackService.updateFeedback(feedbackId, feedback)
+    dispatch({
+      type: feedbackType.FEEDBACK_UPDATED_SUCCESS,
+      data: res.data,
+    })
 
-      history.push("/feedbacks");
-    } catch (error) {
-      dispatch({
-        type: feedbackType.FEEDBACK_ACTION_FAILURE,
-        data: error,
-      });
-      dispatch(
-        setAlert("Failed to update feedback: " + error, severity.Error, 15)
-      );
-    }
-  };
+    history.push('/feedbacks')
+  } catch (error) {
+    dispatch({
+      type: feedbackType.FEEDBACK_ACTION_FAILURE,
+      data: error,
+    })
+    dispatch(setAlert('Failed to update feedback: ' + error, severity.Error, 15))
+  }
+}
