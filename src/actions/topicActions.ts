fad5506d296bb2463dd203197db9a33f5c3e0c27
Amal Kayed <amal.kayed88@gmail.com>
2022-06-28 10:39:13 +0300

    Formatted topicActions code and fixed dispatch types of fetchAllTopics function

--- src/actions/topicActions.ts
+++ src/actions/topicActions.ts
@@ -1,8 +1,9 @@
-import topicService from "../services/topicService";
-import { topicType } from "../types/types";
-import { Topic, severity } from "../types/types";
-import { setAlert } from "./alertActions";
-import history from "../utils/history";
+import topicService from '../services/topicService'
+import { Topic, topicType, severity } from '../types/types'
+import { setAlert } from './alertActions'
+import history from '../utils/history'
+import { Dispatch } from 'redux'
+import { TopicActionFailure, TopicGetAllRequest, TopicGetAllSuccess } from '../types/state'
 
 /**
  * Create topic
@@ -15,22 +16,22 @@ export const createTopic = (topic: Topic) => async (dispatch: any) => {
     dispatch({
       type: topicType.TOPIC_CREATED_REQUEST,
       data: topic,
-    });
+    })
 
-    const { data } = await topicService.createTopic(topic);
+    const { data } = await topicService.createTopic(topic)
     dispatch({
       type: topicType.TOPIC_CREATED_SUCCESS,
       data,
-    });
-    dispatch(setAlert("Topic created successfully!"));
+    })
+    dispatch(setAlert('Topic created successfully!'))
   } catch (e) {
     dispatch({
       type: topicType.TOPIC_ACTION_FAILURE,
       data: e,
-    });
-    dispatch(setAlert("Failed to create the topic: " + e, severity.Error, 15));
+    })
+    dispatch(setAlert('Failed to create the topic: ' + e, severity.Error, 15))
   }
-};
+}
 
 /**
  * @function
@@ -40,39 +41,39 @@ export const deleteTopic = (id: string) => async (dispatch: any) => {
   try {
     dispatch({
       type: topicType.TOPIC_DELETED_REQUEST,
-    });
-    const data = await topicService.deleteTopic(id);
-    dispatch({ type: topicType.TOPIC_DELETED_SUCCESS, data: { _id: id } });
+    })
+    const data = await topicService.deleteTopic(id)
+    dispatch({ type: topicType.TOPIC_DELETED_SUCCESS, data: { _id: id } })
   } catch (e) {
     dispatch({
       type: topicType.TOPIC_ACTION_FAILURE,
       data: e,
-    });
-    dispatch(setAlert("Failed to delete the topic!: " + e, severity.Error, 15));
+    })
+    dispatch(setAlert('Failed to delete the topic!: ' + e, severity.Error, 15))
   }
-};
+}
 
 /**
  * @function
  * @desc Fetches all topics.
  */
-export const fetchAllTopics = () => async (dispatch: any) => {
-  try {
-    dispatch({
-      type: topicType.TOPIC_GETALL_REQUEST,
-    });
-    const res = await topicService.fetchAllTopics();
-    dispatch({ type: topicType.TOPIC_GETALL_SUCCESS, data: res.data });
-  } catch (e) {
-    dispatch({
-      type: topicType.TOPIC_ACTION_FAILURE,
-      data: e,
-    });
-    dispatch(
-      setAlert("Failed to fetch all the topics!: " + e, severity.Error, 15)
-    );
+export const fetchAllTopics =
+  () =>
+  async (dispatch: Dispatch<TopicGetAllRequest | TopicGetAllSuccess | TopicActionFailure>) => {
+    try {
+      dispatch({
+        type: topicType.TOPIC_GETALL_REQUEST,
+      })
+      const res = await topicService.fetchAllTopics()
+      dispatch({ type: topicType.TOPIC_GETALL_SUCCESS, data: res.data })
+    } catch (e) {
+      dispatch({
+        type: topicType.TOPIC_ACTION_FAILURE,
+        data: e as string,
+      })
+      setAlert('Failed to fetch all the topics!: ' + e, severity.Error, 15)(dispatch)
+    }
   }
-};
 
 /**
  * @function
@@ -82,41 +83,36 @@ export const fetchTopicById = (id: string) => async (dispatch: any) => {
   try {
     dispatch({
       type: topicType.TOPIC_GET_CURRENT_REQUEST,
-    });
-    const res = await topicService.fetchTopicById(id);
-    dispatch({ type: topicType.TOPIC_GET_CURRENT_SUCCESS, data: res.data });
+    })
+    const res = await topicService.fetchTopicById(id)
+    dispatch({ type: topicType.TOPIC_GET_CURRENT_SUCCESS, data: res.data })
   } catch (error) {
     dispatch({
       type: topicType.TOPIC_ACTION_FAILURE,
       data: error,
-    });
-    dispatch(
-      setAlert("Failed to fetch the topic: " + error, severity.Error, 15)
-    );
+    })
+    dispatch(setAlert('Failed to fetch the topic: ' + error, severity.Error, 15))
   }
-};
+}
 
 /**
  * @function
  * @desc update topic.
  */
-export const updateTopic =
-  (topicId: string, topic: Topic) => async (dispatch: any) => {
-    try {
-      dispatch({
-        type: topicType.TOPIC_UPDATED_REQUEST,
-      });
+export const updateTopic = (topicId: string, topic: Topic) => async (dispatch: any) => {
+  try {
+    dispatch({
+      type: topicType.TOPIC_UPDATED_REQUEST,
+    })
 
-      const res = await topicService.updateTopic(topicId, topic);
-      dispatch({ type: topicType.TOPIC_UPDATED_SUCCESS, data: res.data });
-      history.push("/topics");
-    } catch (error) {
-      dispatch({
-        type: topicType.TOPIC_ACTION_FAILURE,
-        data: error,
-      });
-      dispatch(
-        setAlert("Failed to update topic: " + error, severity.Error, 15)
-      );
-    }
-  };
+    const res = await topicService.updateTopic(topicId, topic)
+    dispatch({ type: topicType.TOPIC_UPDATED_SUCCESS, data: res.data })
+    history.push('/topics')
+  } catch (error) {
+    dispatch({
+      type: topicType.TOPIC_ACTION_FAILURE,
+      data: error,
+    })
+    dispatch(setAlert('Failed to update topic: ' + error, severity.Error, 15))
+  }
+}
