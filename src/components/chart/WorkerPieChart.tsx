f51cd2ccc0a9e4bc6c55c945fa410573bd49c6c1
Amal Kayed <amal.kayed88@gmail.com>
2022-07-09 15:47:59 +0300

    Implementation allows workers to get analysis of their own emotions

---
+++ src/components/chart/WorkerPieChart.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+import { fetchMyFeelings } from '../../actions/myFeelingActions';
+import { IRootState } from '../../utils/store';
+
+type Props = {
+    cx: number,
+    cy: number, 
+    midAngle: number, 
+    innerRadius: number, 
+    outerRadius: number, 
+    percent: number,
+  }
+
+export const WorkerPieChart: React.FC = () => {
+      
+const dispatch = useDispatch();
+
+const feelingsChartData = useSelector((state: IRootState) => {
+    
+    const { myFeelings } = state.myFeeling;
+
+    const chartData : {[key: number]: number; } = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    };
+
+    const feelingNames = [
+        'Dissatisfied',
+        'Unhappy',
+        'Satisfied',
+        'Happy',
+        'Quite happy'
+    ];
+
+    myFeelings.forEach(feeling => {
+        if (feeling.anxious) chartData[feeling.anxious] += 1;
+        if (feeling.comfortable) chartData[feeling.comfortable] += 1;
+        if (feeling.energetic) chartData[feeling.energetic] += 1;
+        if (feeling.enthusiastic) chartData[feeling.enthusiastic] += 1;
+        if (feeling.frustrated) chartData[feeling.frustrated] += 1;
+        if (feeling.satisfied) chartData[feeling.satisfied] += 1;
+        if (feeling.stressed) chartData[feeling.stressed] += 1;
+    });
+    
+    return Object.entries(chartData).map(entry => ({name: feelingNames[parseInt(entry[0]) - 1], value: entry[1]}));
+});
+  console.log(feelingsChartData);
+  useEffect(() => {
+    dispatch(fetchMyFeelings());
+  }, [dispatch]);
+
+  
+const COLORS = ['#bf2608', '#FFBB28', '#FF8042', '#0088FE', '#00C49F'];
+const RADIAN = Math.PI / 180;
+  
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: Props) => {
+    if (!percent) return null;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+         {`${(percent * 100).toFixed(0)}%`}
+    </text>
+    );
+};
+
+return (
+        <ResponsiveContainer width="90%" height="90%">
+        <PieChart width={300} height={300}>
+            <Pie
+            data={feelingsChartData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+        >
+            {feelingsChartData.map((_entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+        </PieChart>
+    </ResponsiveContainer>
+    );
+};
+
+export default WorkerPieChart;
