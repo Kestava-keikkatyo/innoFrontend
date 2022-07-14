11823fc260b35d6c1b16c6e219e3cf6f511392eb
Oliver Östman <oliver.ostman@metropolia.fi>
2022-07-14 16:06:15 +0300

    Updated/Changed landing page toolbar to a responsive one

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -6,10 +6,15 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Grid
+  Grid,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
 } from '@mui/material';
 import SwipeableViews from 'react-swipeable-views';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 import FooterPage from './FooterPage';
 import FirstLandingPage from './FirstLandingPage';
 import ContentLifeSpan from './ContentWorkLifeSpan';
@@ -32,6 +37,8 @@ const LandingPage = () => {
   const [activeStep, setActiveStep] = useState(0);
   const { t } = useTranslation();
 
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
@@ -44,17 +51,26 @@ const LandingPage = () => {
     setActiveStep(step);
   };
 
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <div className='icd' >
       <AppBar position="fixed" elevation={1} className="landing-appbar">
+        {/* Old Toolbar
         <Toolbar className="toolbar" variant="dense">
           <Typography>
             <Link className="landing-logo" to="/">
               KEIKKAKAVERI
             </Link>
           </Typography>
-          {/*<img className="bw-logo2" src={logo} alt="keikkakaveri_Logo" />*/}
-          <Grid container>
+          {/*<img className="bw-logo2" src={logo} alt="keikkakaveri_Logo" /> */} {/* Old Toolbar continues
+          <Grid style={{display: "flex"}}>
             <Typography>
               <Link className="landing-nav-link" to="/home">
                 {t('tyopoyta')}
@@ -68,13 +84,13 @@ const LandingPage = () => {
           </Grid>
           <Typography>
               <Button
-              style={{left: "-30%", marginTop:"20%"}}
+              style={{left: '-30%', marginTop: '20%'}}
               onClick={() => changeLanguage('fi')}
               >
               <img src={fi1} alt={"fi"} style={{ width: '30px' }} />
             </Button>
             <Button
-              style={{left:"20%",marginTop:"-55%"}}
+              style={{left: '20%', marginTop: '-55%'}}
               onClick={() => changeLanguage('en')}
             >
               <img src={us1} alt={"en"} style={{ width: '30px' }} />
@@ -85,6 +101,141 @@ const LandingPage = () => {
               {t('kirjaudu_sisaan')}
             </Link>
           </Typography>
+        </Toolbar>
+        */}
+        <Toolbar className="toolbar">
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 4,
+              display: { xs: 'none', sm: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <Link className="landing-logo" to="/">
+              KEIKKAKAVERI
+            </Link>
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="default"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+              }}
+            > {/* Side menu */}
+                <MenuItem key="0" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link className="landing-nav-link" to="/home">
+                      {t('tyopoyta')}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+                <MenuItem key="1" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link className="landing-nav-link2" to="/databank">
+                      {t('tietopankki')}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+                <MenuItem key="2" onClick={handleCloseNavMenu}>
+                  <Button
+                    onClick={() => changeLanguage('fi')} >
+                    <img src={fi1} alt={"fi"} style={{ width: '30px' }} />
+                  </Button>
+                </MenuItem>
+                <MenuItem key="3" onClick={handleCloseNavMenu}>
+                  <Button
+                    onClick={() => changeLanguage('en')} >
+                    <img src={us1} alt={"en"} style={{ width: '30px' }} />
+                  </Button>
+                </MenuItem>
+                <MenuItem>
+                  <Typography textAlign="center">
+                    <Link className="landing-login" to="/login">
+                      {t('kirjaudu_sisaan')}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', sm: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <Link className="landing-logo" to="/">
+              KEIKKAKAVERI
+            </Link>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
+            <Typography sx={{ my: 3, display: 'block'}}>
+              <Link className="landing-nav-link" to="/home">
+                {t('tyopoyta')}
+              </Link>
+            </Typography>
+            <Typography sx={{ my: 3, display: 'block'}}>
+              <Link className="landing-nav-link2" to="/databank">
+                {t('tietopankki')}
+              </Link>
+            </Typography>
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: {xs: 'none', sm: 'flex' } }}>
+            <Button onClick={() => changeLanguage('fi')}
+              sx={{ display: 'block', paddingRight: '0' }} >
+                <img src={fi1} alt={"fi"} style={{ width: '30px' }} />
+            </Button>
+            <Button onClick={() => changeLanguage('en')}
+              sx={{ display: 'block', paddingLeft: '0' }} >
+                <img src={us1} alt={"en"} style={{ width: '30px' }} />
+            </Button>
+            <Typography sx={{ my: 3, display: 'block', marginLeft: '1rem' }} >
+              <Link className="landing-login" to="/login">
+                {t('kirjaudu_sisaan')}
+              </Link>
+            </Typography>
+          </Box>
         </Toolbar>
       </AppBar>
       <MobileStepper
