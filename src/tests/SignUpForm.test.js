10f9b69e100980e9dddeb973224715b2977d0517
Eric <55787260+Mahamurahti@users.noreply.github.com>
2022-09-14 13:03:29 +0300

    Fix SignUpForm location

--- src/tests/SignUpForm.test.js
+++ src/tests/SignUpForm.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
 import { fireEvent, render, wait } from "@testing-library/react"
-import SignUpForm from "../pages/LandingPage/SignUpForm"
+import SignUpForm from "../pages/LoginPage/SignUpForm"
 
 describe("<LogInForm />", () => {
   let component
