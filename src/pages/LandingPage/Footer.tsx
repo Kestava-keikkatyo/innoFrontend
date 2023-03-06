35355a0c0b0e8484aafa3c6d58fb92cefd771d53
timotth <timo.holopainen2@metropolia.fi>
2023-03-06 12:14:22 +0200

    landing page footer improvements, eslint suggestion fixes

--- src/pages/LandingPage/Footer.tsx
+++ src/pages/LandingPage/Footer.tsx
@@ -6,6 +6,7 @@ import footer_logo from '../../assets/logo_keikkakaveri_footteri.svg'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
+// Landing page footer
 const Footer = () => {
     const { t } = useTranslation();
 
@@ -23,18 +24,18 @@ const Footer = () => {
 
     return (
         <ThemeProvider theme={fontTheme}>
-            <Container style={{ backgroundColor: '#C0CFFA', maxWidth: 'none', paddingTop: '32px' }}>
-                <Grid container spacing={4} style={{ paddingLeft: '20%', paddingRight: '20%' }}>
-                    <Grid item xs={12} sm={12} md={4}>
+            <Container style={{ backgroundColor: '#C0CFFA', maxWidth: 'none', paddingTop: '32px', justifyContent: 'center', display: 'flex' }}>
+                <Grid container spacing={4} style={{ maxWidth: '1170px' }}>
+                    <Grid className='content-center' item xs={12} sm={12} md={4}>
                         <img src={footer_logo} alt='Keikkakaveri logo' style={{ maxWidth: '200px', marginBottom: '8px' }}></img>
                         <Typography>{t('keikkakaveri_info')}</Typography>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={4}>
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                            <Link to="/databank" style={{ textDecoration: 'none' }}>
+                    <Grid className='content-center' item xs={12} sm={12} md={4}>
+                        <div className='content-center' style={{ display: 'flex', flexDirection: 'column' }}>
+                            <Link to="/databank" style={{ textDecoration: 'none', maxWidth: 'fit-content' }}>
                                 <Typography>{t('databank_link')}</Typography>
                             </Link>
-                            <Link to="/login" style={{ textDecoration: 'none' }}>
+                            <Link to="/login" style={{ textDecoration: 'none', maxWidth: 'fit-content' }}>
                                 <Typography style={{ marginTop: '8px' }}>{t('keikkakaveri_tool_link')}</Typography>
                             </Link>
                             <div style={{ display: 'flex', alignItems: 'center', marginTop: '8px' }}>
@@ -43,8 +44,8 @@ const Footer = () => {
                             </div>
                         </div>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={4}>
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Grid className='content-center' item xs={12} sm={12} md={4}>
+                        <div className='content-center' style={{ display: 'flex', flexDirection: 'column' }}>
                             <div style={{ display: 'flex', alignItems: 'center' }}>
                                 <img src={link_out} alt='Link out icon' style={{ width: 'auto', height: '24px', marginRight: '8px' }}></img>
                                 <a href="https://www.metropolia.fi/fi/tutkimus-kehitys-ja-innovaatiot/hankkeet/kestava-keikkatyo-hanke" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
