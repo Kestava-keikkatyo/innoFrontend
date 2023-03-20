bcb1d0ee3da90dca4a83fb1c4674f946257f3f71
timotth <timo.holopainen2@metropolia.fi>
2023-03-20 18:55:35 +0200

    revert last changes

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -2,10 +2,7 @@
 let baseUrl = 'http://localhost:3001/api';
 
 if(process.env.NODE_ENV === 'production'){
-  baseUrl = 'http://18.197.146.251:3001/api';
+  baseUrl = 'http://3.65.218.23:3001/api';
 }
 
-// if(process.env.NODE_ENV === 'production'){
-//   baseUrl = 'http://3.65.218.23:3001/api';
-// }
  export default baseUrl;
