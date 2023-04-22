90f113af8e51e9074d4c222d8863a558889a3897
unknown <jasmin.janka@metropolia.fi>
2023-04-22 14:44:13 +0300

    Fix

--- src/pages/Profile/Workers.tsx
+++ src/pages/Profile/Workers.tsx
@@ -1,13 +1,15 @@
+
 import { DataGrid, GridColumns } from '@mui/x-data-grid';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { IRootState } from '../../utils/store';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllWorkers } from '../../actions/usersActions';
 import makeStyles from '@mui/styles/makeStyles';
 import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next'
 import i18next from 'i18next'
-import { E_SET_CURRENT } from '../../types/state';
 import { ThemeProvider, createTheme } from '@mui/material';
 
 const Workers: React.FC = () => {
@@ -15,9 +17,12 @@ const Workers: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { users } = useSelector((state: IRootState) => state.users || []);
-  const users = useSelector((state: IRootState) => state.user.contacts)
-  const currentForm = useSelector((state: any) => state.employmentAgreements.currentAgreement);
-  
+  useEffect(() => {
+    dispatch(fetchAllWorkers());
+  }, [dispatch]);
+  let rows = [];
+  rows = users;
+
   const theme = createTheme({
     typography: {
       fontFamily: 'Montserrat, serif',
@@ -27,29 +32,7 @@ const Workers: React.FC = () => {
       },
     },
   });
-
-  useEffect(() => {
-    dispatch(fetchAllWorkers());
-  }, [dispatch]);
   
-  const workers: any[] = []
-
-  if (users[0]) {
-    users.forEach((user) => {
-      if (user.userType == "worker") {
-        workers.push(user)
-      }
-    }) 
-  }
-  
-  const setEmploymentFormWorker = (id: any) => {
-    const employmentForm = { ...currentForm, worker: id }
-    dispatch({ type: E_SET_CURRENT, data: employmentForm})
-  };
-  
-  let rows = [];
-  rows = workers;
-
   const columns: GridColumns = [
     {
       field: 'name',
@@ -94,56 +77,49 @@ const Workers: React.FC = () => {
       flex: 1,
       headerClassName: 'super-app-theme--header',
       renderCell: (params) => {
-        return (
-          <>
-            <Link to={'/workers/profile/' + params.id}>{t('list_profile')}</Link>
-          </>
-        );
-      },
-        <>
-        <Link className={classes.link} to={'/workers/profile/' + params.id}>{t('list_profile')}</Link>
-
-        { (userType === "agency") &&
-          <Link to={'/employment/'} onClick={() => setEmploymentFormWorker(params.id)}>{t('employ')}</Link>
-        }
+      return (
+      <>
+      <Link to={'/workers/profile/' + params.id}>{t('list_profile')}</Link>
       </>
       );
     },
-  ];
+  },
+];
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div style={{ height: '75vh', width: '100%', padding: '0 1rem' }}>
-        <div>
-          <Typography className={'header'}
-            style={{ marginTop: '25px', marginBottom: '15px' }}
-            variant="h1">
-            {t('list_title_workers')}</Typography>
-        </div>
-        <DataGrid
-          sx={{
-            '& .super-app-theme--header': {
-              backgroundColor: '#C0CFFA',
-              borderRight: '3px solid white',
-            },
-            '.MuiDataGrid-columnSeparator': {
-              display: 'none',
-            },
-            '&.MuiDataGrid-root': {
-              border: 'none',
-            },
-          }}
-          style={{ marginTop: '20px', border: '3px solid #C0CFFA', borderRadius: '0' }}
-          getRowId={(row) => row._id}
-          rows={rows}
-          disableSelectionOnClick
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-        />
-      </div>
-    </ThemeProvider>
-  );
+return (
+  <ThemeProvider theme={theme}>
+<div style={{ height: '75vh', width: '100%', padding: '0 1rem' }}>
+  <div>
+    <Typography className={'header'}
+                style={{marginTop: '25px', marginBottom: '15px'}}
+                variant="h1">
+      {t('list_title_workers')}</Typography>
+  </div>
+  <DataGrid
+  
+   sx={{
+    '& .super-app-theme--header': {
+      backgroundColor: '#C0CFFA',
+      borderRight: '3px solid white',
+    },
+    '.MuiDataGrid-columnSeparator': {
+      display: 'none',
+    },
+    '&.MuiDataGrid-root': {
+      border: 'none',
+    },
+  }}
+  style={{ marginTop: '20px', border: '3px solid #C0CFFA', borderRadius: '0' }}
+      getRowId={(row) => row._id}
+      rows={rows}
+      disableSelectionOnClick
+      columns={columns}
+      pageSize={10}
+      rowsPerPageOptions={[10]}
+  />
+</div>
+</ThemeProvider>
+);
 }
 
 const useStyles = makeStyles(() => ({
@@ -162,9 +138,6 @@ const useStyles = makeStyles(() => ({
     objectFit: 'cover',
     marginRight: '10px',
   },
-  link: {
-    marginRight: '6px'
-  }
 }));
 
 export default Workers;
